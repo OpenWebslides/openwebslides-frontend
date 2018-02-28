e60e984c5978bf344bf6cab6cb9ba0f3d03d7adc
Jasper D'haene <jasper.dhaene@gmail.com>
2018-02-28 15:09:40 +0100

    semantic UI integration

--- app/pages/components/HomePage.js
+++ app/pages/components/HomePage.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { translate } from 'react-i18next';
 import type { TranslatorProps } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 type Props = TranslatorProps & { /* new props go here */ };
 
@@ -14,7 +15,7 @@ const HomePage = (props: Props): React.Node => {
     <div>
       <p><Link to="/topics">Topics page</Link></p>
       <p>
-        <button>{t('common:button.cancel')}</button> {`<-- This doesn't do anything!`}
+        <Button>{t('common:button.cancel')}</Button> {`<-- This doesn't do anything!`}
       </p>
     </div>
   );
