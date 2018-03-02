3fdeacb207367f2584a41ae15fe5cfc1e12ee64a
Dromin <contact@dromin.be>
2018-03-02 10:56:59 +0100

    Set up test framework & added example test for the generate-random-string lib.

---
+++ app/lib/generate-random-string/test/index.test.js
@@ -0,0 +1,13 @@
+// @flow
+
+import generateRandomString from '..';
+
+describe('generateRandomString', (): void => {
+  it('returns a string of the correct length', (): void => {
+    expect(generateRandomString(2).length).toBe(2);
+    expect(generateRandomString(8).length).toBe(8);
+    expect(generateRandomString(10).length).toBe(10);
+    expect(generateRandomString(20).length).toBe(20);
+    expect(generateRandomString(100).length).toBe(100);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// @flow
+
+module.exports = {
+  // Print coverage report when running tests.
+  collectCoverage: true,
+  // Only collect coverage for the files in the /app folder.
+  collectCoverageFrom: [
+    'app/**/*.js',
+  ],
+};
