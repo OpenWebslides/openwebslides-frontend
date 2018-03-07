7f339f4fee3a6f682e7fca25ce8a8ce4727bb031
Jasper D'haene <jasper.dhaene@gmail.com>
2018-03-07 10:00:52 +0100

    added avatars + improved social feed + removed prop errors + rm searchSection

--- app/modules/home/components/index.js
+++ app/modules/home/components/index.js
@@ -1,12 +1,10 @@
 // @flow
 
 import HomeContainer from './HomeContainer';
-import HomeSearchSection from './HomeSearchSection';
 import HomeSocialFeed from './HomeSocialFeed';
 
 export const components = {
   homecontainer: HomeContainer,
-  homesearchsection: HomeSearchSection,
   homesocialfeed: HomeSocialFeed,
 };
 
--- app/pages/components/HomePage.js
+++ app/pages/components/HomePage.js
@@ -22,52 +22,34 @@ const MenuRight = (): React.Node => {
         <strong>Professor X</strong>
       </Menu.Item>
       <Menu.Item>
-        <Icon name="user" size="large" circular="true" inverted="true" />
+        <Icon name="user" size="large" circular={true} inverted={true} />
       </Menu.Item>
     </Menu.Menu>
   );
 };
-
-/* TODO passing t prop not possible yet
-const MenuLeft = (props: Props): React.Node => {
-  const { t } = props;
-
-  return (
-    <div>
-      <Menu.Item icon="true">
-        <Image size="tiny" src="/assets/images/logo.png" />
-        {t('app:title')}
-      </Menu.Item>
-      <Menu.Item>
-        <Link to="/topics">Topics page</Link>
-      </Menu.Item>
-    </div>
-  );
-};
-*/
 
 const HomePage = (props: Props): React.Node => {
   const { t } = props;
 
   return (
     <div>
-      <Menu secondary="true" attached="top">
-        <Menu.Item icon="true">
+      <Menu secondary={true} attached="top">
+        <Menu.Item icon={true}>
           <Image size="tiny" src="/assets/images/logo.png" href="/" />
           {t('app:title')}
         </Menu.Item>
         <MenuRight />
       </Menu>
-      <Grid stretched="true">
-        <Grid.Column stretched="true" width={2}>
-          <Menu attached="left" vertical="true" fluid="true">
+      <Grid stretched={true}>
+        <Grid.Column stretched={true} width={2}>
+          <Menu vertical={true} fluid={true}>
             <Menu.Header>
-              <Icon name="book" as={Link} to="/topics" size="large" link="true" />
+              <Link to="/topics">Library</Link>
             </Menu.Header>
           </Menu>
         </Grid.Column>
 
-        <Grid.Column stretched="true" width={14}>
+        <Grid.Column stretched={true} width={14}>
           <HomeContainer />
         </Grid.Column>
       </Grid>
