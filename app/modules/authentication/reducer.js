e4b964579a2675feeb6c171147514c017f486dc4
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-03-13 14:35:12 +0100

    Add auth reducer

---
+++ app/modules/authentication/reducer.js
@@ -0,0 +1,71 @@
+// @flow
+
+import { dummyAuthentication } from 'data/dummyData';
+
+import * as t from './actionTypes';
+import type { Account, AuthState } from './model';
+
+const initialState: AuthState = dummyAuthentication;
+
+const signin = (state: AuthState, action: t.SigninEmailAction | t.SigninOAuthAction): AuthState => {
+  const { email } = action.payload;
+
+  const account: Account = {
+    id: 'baz',
+    email,
+    firstName: 'John',
+    lastName: 'Doe',
+  };
+
+  return {
+    ...state,
+    account,
+  };
+};
+
+const signup = (state: AuthState, action: t.SignupAction): AuthState => {
+  const { email, firstName, lastName } = action.payload;
+
+  const account: Account = {
+    id: 'baz',
+    email,
+    firstName,
+    lastName,
+  };
+
+  return {
+    ...state,
+    account,
+  };
+};
+
+const signout = (state: AuthState): AuthState => {
+  return {
+    ...state,
+    account: null,
+  };
+};
+
+const reducer = (state: AuthState = initialState, action: t.AuthenticationAction): AuthState => {
+  switch (action.type) {
+    case t.SIGNIN_EMAIL:
+    case t.SIGNIN_OAUTH:
+      return signin(state, action);
+    case t.SIGNUP:
+      return signup(state, action);
+    case t.SIGNOUT:
+      return signout(state);
+    case t.SIGNIN_EMAIL_ERROR:
+    case t.SIGNIN_OAUTH_ERROR:
+    case t.SIGNUP_ERROR:
+    case t.SIGNOUT_ERROR:
+      return state;
+    default:
+      // Type error when not all action.type cases are handled.
+      // eslint-disable-next-line no-unused-expressions
+      (action: empty);
+      return state;
+  }
+};
+
+export default reducer;
