661155fa6c7664562b5561cb951d0c2ec899e385
Jasper D'haene <jasper.dhaene@gmail.com>
2018-03-14 15:32:56 +0100

    added hardcoded library topics to store

--- app/modules/topics/dummyData.js
+++ app/modules/topics/dummyData.js
@@ -33,4 +33,22 @@ export const dummyTopicsById: TopicsById = {
     description: 'Such wisdom. Much internet',
     rootContentItemId: 'nvlg2u0p1h',
   },
+  fffyhnd0w0: {
+    id: 'fffyhnd0w0',
+    title: 'Introduction to mutations in modern biology',
+    description: 'This is an introductory course on the subject of mutations. It should help you understand how genetic evolution works and why mutants, like the famous X-men, exist',
+    rootContentItemId: 'q4lg2u0p78',
+  },
+  effhrl5gvy: {
+    id: 'effhrl5gvy',
+    title: 'Y U DO DIS: An analysis of modern slang',
+    description: 'This is an introductory course on the subject of modern slang. It should help the reader to better understand teens, as well as peoplez of the internet, and how they communicate. Beware: course contains dank memes',
+    rootContentItemId: 'a4lhct0p78',
+  },
+  feeyhss0w0: {
+    id: 'feeyhss0w0',
+    title: 'The cultural impact of Dragon Ball Z on youths today',
+    description: 'This is an introductory course on the subject of DBZ and its cultural impact. Beware: this is a Work In Progress. Or better yet: rejoice, because this isn\'t even the topic\'s FINAL FORM!',
+    rootContentItemId: 'b4lg245148',
+  },
 };
