858c9f6676890b930ee360388d611f25aa6c84cb
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-03-19 09:42:54 +0100

    Remove Semantic UI CSS version

--- app/index.js
+++ app/index.js
@@ -9,8 +9,6 @@ import { Provider } from 'react-redux';
 
 import configureStore from 'store/configureStore';
 
-import 'semantic-ui-css/semantic.min.css';
-
 // The uppermost app container component.
 import Application from './Application';
 
