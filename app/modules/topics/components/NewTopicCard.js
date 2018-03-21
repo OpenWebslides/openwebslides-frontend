b312eaa9d859242e4bfbce690c04cc46acb20c3e
Jasper D'haene <jasper.dhaene@gmail.com>
2018-03-21 15:16:28 +0100

    styled newTopicCard

--- app/modules/topics/components/NewTopicCard.js
+++ app/modules/topics/components/NewTopicCard.js
@@ -11,7 +11,7 @@ import { Form, Button } from 'semantic-ui-react';
 import { add } from '../actions';
 
 type DispatchProps = {
-  onAddButtonClick: (string) => void,
+  onAddButtonClick: (string, string) => void,
 };
 
 type Props = TranslatorProps & DispatchProps & FormProps;
@@ -26,24 +26,37 @@ const mapDispatchToProps = (dispatch: Dispatch<*>): DispatchProps => {
   };
 };
 
-const NewTopicCardForm = (props: TranslatorProps): React.Node => {
+const NewTopicCardForm = (props: Props): React.Node => {
   const {
     t,
     handleSubmit,
   } = props;
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <label htmlFor="title">
-        {t('common:form.title')}
-        <Field id="title" name="title" component="input" type="text" />
-      </label>
+    <Form onSubmit={handleSubmit} size="large">
+      <Form.Field width="6">
+        <label htmlFor="title" control={Form.Input}>
+          {t('common:form.title')}
+          <Field
+            id="title"
+            component={Form.Input}
+            name="title"
+            placeholder={t('common:form.title')}
+          />
+        </label>
+      </Form.Field>
 
-
-      <label htmlFor="description">
-        {t('common:form.description')}
-        <Field id="description" name="description" component="input" type="text" />
-      </label>
+      <Form.Field width="6">
+        <label htmlFor="description" control="textarea">
+          {t('common:form.description')}
+          <Field
+            id="description"
+            component="textarea"
+            name="description"
+            placeholder={t('common:form.description')}
+          />
+        </label>
+      </Form.Field>
 
       <Form.Group>
         <Form.Field id="form-button-control-public" control={Button}>
@@ -65,7 +78,8 @@ const PureNewTopicCard = (props: Props): React.Node => {
     onAddButtonClick,
   } = props;
 
-  const handleSubmit = (values: string): void => {
+  // TODO: the flow type for values might be a bit dodgy
+  const handleSubmit = (values: { +[values: * ]: string }): void => {
     onAddButtonClick(values.title, values.description);
   };
 
