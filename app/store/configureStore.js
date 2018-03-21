7ec7019f184e5340a09644ccfc81f2a9287d900c
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-03-21 14:48:28 +0100

    Move Redux Form reducer to rootReducer

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -3,27 +3,20 @@
  * Sets up the redux store, including middelware and access for redux-devtools browser extension.
  */
 
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import type { Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { reducer as formReducer } from 'redux-form';
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
 const configureStore = (): Store<*, *> => {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    combineReducers({
-      rootReducer,
-      form: formReducer,
-    }),
-    composeWithDevTools(
-      applyMiddleware(sagaMiddleware),
-    ),
-  );
+  const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  ));
   sagaMiddleware.run(rootSaga);
 
   return store;
