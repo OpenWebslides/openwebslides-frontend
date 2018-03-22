75927861311ed8761209aa562064061426a384cd
Jasper D'haene <jasper.dhaene@gmail.com>
2018-03-22 14:16:38 +0100

    added dummy topics referenced in feedItems

--- app/modules/topics/dummyData.js
+++ app/modules/topics/dummyData.js
@@ -15,4 +15,22 @@ export const dummyTopics: TopicsState = {
     description: '',
     rootContentItemId: 'qyrgv0bcd6',
   },
+  aaaaaaaaa0: {
+    id: 'aaaaaaaaa0',
+    title: 'Hitchhiker\'s guide to the galaxy',
+    description: 'Cultural impact of a funny book',
+    rootContentItemId: 'jdlg2u0p1h',
+  },
+  aaaaaaaaaa: {
+    id: 'aaaaaaaaaa',
+    title: 'How to know your cat is secretly plotting to murder you.',
+    description: '',
+    rootContentItemId: 'lmrgv0bcd6',
+  },
+  bbbbbbbbbb: {
+    id: 'bbbbbbbbbb',
+    title: 'Cursus internet memes',
+    description: 'Such wisdom. Much internet',
+    rootContentItemId: 'nvlg2u0p1h',
+  },
 };
