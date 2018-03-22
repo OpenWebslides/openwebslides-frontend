2204fde20b020e372b14f92f09b6f61f04a8e17a
Jasper D'haene <jasper.dhaene@gmail.com>
2018-03-22 10:23:49 +0100

    users module: model added

---
+++ app/modules/users/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import * as model from './model';
+
+
+import type { User, UsersState } from './model';
+
+const users = {
+  model,
+};
+
+export type { User, UsersState };
+export default users;
---
+++ app/modules/users/model.js
@@ -0,0 +1,24 @@
+// @flow
+/* eslint-disable import/prefer-default-export */
+
+import type { Identifier } from 'types/model';
+import generateRandomString from 'lib/generate-random-string';
+
+// #TODO optimal id length / generation method?
+const ID_LENGTH = 10;
+
+export type User = {
+  +id: Identifier,
+  +first_name: string,
+  +last_name: ?string,
+  +email: string,
+  +password: string,
+};
+
+export type UsersState = {
+  +[userId: Identifier]: User,
+};
+
+export const generateId = (): Identifier => {
+  return generateRandomString(ID_LENGTH);
+};
