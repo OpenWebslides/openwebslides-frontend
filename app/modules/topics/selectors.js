3d4e22ed8af595cfd8591aa954f01b710a8e18ea
Jasper <jasper.dhaene@hotmail.com>
2018-03-26 15:21:17 +0200

    remove debug code from topic selectors

--- app/modules/topics/selectors.js
+++ app/modules/topics/selectors.js
@@ -14,27 +14,11 @@ export const getById = (state: State, id: Identifier): Topic => {
 };
 
 export const getTitleById = (state: State, id: Identifier): string => {
-  // console.log(getModule(state)[id]);
   return getModule(state)[id].title;
 };
 
-// eslint-disable-next-line
 export const getAllTopicIdsWithUserId = (state: State, userId: Identifier): Array<Identifier> => {
   const topicsById = getModule(state);
-  /* DEBUG
-  console.log('result from getModule()');
-  console.log(topicsById);
-  const keying = Object.keys(topicsById);
-  console.log('result from keying');
-  console.log(keying);
-  const mapping = Object.keys(topicsById).map((key) => topicsById[key]);
-  console.log('result from mapping');
-  console.log(mapping);
-  const result = Object
-  .keys(topicsById).map((key) => topicsById[key]).filter((topic) => topic.userId === userId);
-  console.log('result from filtering');
-  console.log(result);
-  */
 
   // Flow is disabled here because of a long-standing bug: https://github.com/facebook/flow/issues/3067
   return (
@@ -49,12 +33,5 @@ export const getAllTopicIdsWithUserId = (state: State, userId: Identifier): Arra
 
 export const getAll = (state: State): Array<Topic> => {
   const topicsById = getModule(state);
-  /*
-  console.log('result from topicsById()');
-  console.log(topicsById);
-  const mapping = Object.keys(topicsById).map((key) => topicsById[key]);
-  console.log('result from mapping');
-  console.log(mapping);
-  */
   return Object.keys(topicsById).map((key) => topicsById[key]);
 };
