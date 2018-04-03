1493addd8c5440a2de2ce7425bee798d62d15a71
Jasper <jasper.dhaene@hotmail.com>
2018-04-03 14:43:44 +0200

    fix rebasing mistakes

--- app/modules/content-items/components/content-item-display/PlainTextContentItemDisplay.js
+++ app/modules/content-items/components/content-item-display/PlainTextContentItemDisplay.js
@@ -3,12 +3,13 @@
 import * as React from 'react';
 import { translate } from 'react-i18next';
 import type { TranslatorProps } from 'react-i18next';
-import ReactMarkdown from 'react-markdown';
 
 import { Icon, Segment } from 'semantic-ui-react';
 
 import { plainTextContentItemTypes } from '../../model';
 import type { PlainTextContentItem } from '../../model';
+
+import EditableTextContent from '../helpers/EditableTextContent';
 
 type PassedProps = {
   contentItem: PlainTextContentItem,
@@ -16,36 +17,41 @@ type PassedProps = {
 
 type Props = TranslatorProps & PassedProps;
 
-const allowedTypes = [
-  'emphasis',
-  'strong',
-  'inlineCode',
-  'link',
-];
-
 const PurePlainTextContentItemDisplay = (props: Props): React.Node => {
   const { contentItem } = props;
   let iconName: string;
+  let multiline: boolean;
 
   switch (contentItem.type) {
     case plainTextContentItemTypes.PARAGRAPH:
       iconName = 'paragraph';
+      multiline = true;
       break;
     case plainTextContentItemTypes.HEADING:
       iconName = 'header';
+      multiline = false;
       break;
     default:
       iconName = 'warning sign';
+      multiline = false;
   }
 
   return (
     <Segment>
-      <Icon name={iconName} />
-      <ReactMarkdown
-        source={contentItem.text}
-        allowedTypes={allowedTypes}
-        unwrapDisallowed={true}
-      />
+      <div style={{ display: 'table', width: '100%' }}>
+        <div style={{ display: 'table-cell', width: '1%', paddingRight: '1rem' }}>
+          <Icon name={iconName} color="grey" />
+        </div>
+        <div style={{ display: 'table-cell', width: '99%' }}>
+          { /* $FlowFixMe */ }
+          <EditableTextContent
+            multiline={multiline}
+            text={contentItem.text}
+            onActivate={(): void => {}}
+            onDeactivate={(): void => {}}
+          />
+        </div>
+      </div>
     </Segment>
   );
 };
