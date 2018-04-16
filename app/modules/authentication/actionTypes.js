1a085a4a84fee669f54b310c174f8a2822f6ac39
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-16 15:26:29 +0200

    Update authentication actionTypes

--- app/modules/authentication/actionTypes.js
+++ app/modules/authentication/actionTypes.js
@@ -3,14 +3,20 @@
 import type { Error } from 'types/error';
 
 export const SIGNIN_EMAIL: 'auth/SIGNIN_EMAIL' = 'auth/SIGNIN_EMAIL';
-export const SIGNIN_OAUTH: 'auth/SIGNIN_OAUTH' = 'auth/SIGNIN_OAUTH';
-export const SIGNUP: 'auth/SIGNUP' = 'auth/SIGNUP';
-export const SIGNOUT: 'auth/SIGNOUT' = 'auth/SIGNOUT';
+export const SIGNIN_EMAIL_SUCCESS: 'auth/SIGNIN_EMAIL_SUCCESS' = 'auth/SIGNIN_EMAIL_SUCCESS';
+export const SIGNIN_EMAIL_FAILURE: 'auth/SIGNIN_EMAIL_FAILURE' = 'auth/SIGNIN_EMAIL_FAILURE';
 
-export const SIGNIN_EMAIL_ERROR: 'auth/SIGNIN_EMAIL_ERROR' = 'auth/SIGNIN_EMAIL_ERROR';
-export const SIGNIN_OAUTH_ERROR: 'auth/SIGNIN_OAUTH_ERROR' = 'auth/SIGNIN_OAUTH_ERROR';
-export const SIGNUP_ERROR: 'auth/SIGNUP_ERROR' = 'auth/SIGNUP_ERROR';
-export const SIGNOUT_ERROR: 'auth/SIGNOUT_ERROR' = 'auth/SIGNOUT_ERROR';
+export const SIGNIN_OAUTH: 'auth/SIGNIN_OAUTH' = 'auth/SIGNIN_OAUTH';
+export const SIGNIN_OAUTH_SUCCESS: 'auth/SIGNIN_OAUTH_SUCCESS' = 'auth/SIGNIN_OAUTH_SUCCESS';
+export const SIGNIN_OAUTH_FAILURE: 'auth/SIGNIN_OAUTH_FAILURE' = 'auth/SIGNIN_OAUTH_FAILURE';
+
+export const SIGNUP: 'auth/SIGNUP' = 'auth/SIGNUP';
+export const SIGNUP_SUCCESS: 'auth/SIGNUP_SUCCESS' = 'auth/SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE: 'auth/SIGNUP_FAILURE' = 'auth/SIGNUP_FAILURE';
+
+export const SIGNOUT: 'auth/SIGNOUT' = 'auth/SIGNOUT';
+export const SIGNOUT_SUCCESS: 'auth/SIGNOUT_SUCCESS' = 'auth/SIGNOUT_SUCCESS';
+export const SIGNOUT_FAILURE: 'auth/SIGNOUT_FAILURE' = 'auth/SIGNOUT_FAILURE';
 
 export type SigninEmailAction = {
   type: typeof SIGNIN_EMAIL,
@@ -41,32 +47,62 @@ export type SignoutAction = {
   type: typeof SIGNOUT,
 };
 
-export type SigninEmailErrorAction = {
-  type: typeof SIGNIN_EMAIL_ERROR,
+export type SigninEmailSuccessAction = {
+  type: typeof SIGNIN_EMAIL_SUCCESS,
+};
+
+export type SigninOAuthSuccessAction = {
+  type: typeof SIGNIN_OAUTH_SUCCESS,
+};
+
+export type SignupSuccessAction = {
+  type: typeof SIGNUP_SUCCESS,
   error: Error,
 };
 
-export type SigninOAuthErrorAction = {
-  type: typeof SIGNIN_OAUTH_ERROR,
+export type SignoutSuccessAction = {
+  type: typeof SIGNOUT_SUCCESS,
   error: Error,
 };
 
-export type SignupErrorAction = {
-  type: typeof SIGNUP_ERROR,
+export type SigninEmailFailureAction = {
+  type: typeof SIGNIN_EMAIL_FAILURE,
   error: Error,
 };
 
-export type SignoutErrorAction = {
-  type: typeof SIGNOUT_ERROR,
+export type SigninOAuthFailureAction = {
+  type: typeof SIGNIN_OAUTH_FAILURE,
   error: Error,
 };
+
+export type SignupFailureAction = {
+  type: typeof SIGNUP_FAILURE,
+  error: Error,
+};
+
+export type SignoutFailureAction = {
+  type: typeof SIGNOUT_FAILURE,
+  error: Error,
+};
+
+export type SigninSuccessAction =
+  | SigninEmailSuccessAction
+  | SigninOAuthSuccessAction;
+
+export type SigninFailureAction =
+  | SigninEmailFailureAction
+  | SigninOAuthFailureAction;
 
 export type AuthenticationAction =
   | SigninEmailAction
   | SigninOAuthAction
   | SignupAction
   | SignoutAction
-  | SigninEmailErrorAction
-  | SigninOAuthErrorAction
-  | SignupErrorAction
-  | SignoutErrorAction;
+  | SigninEmailSuccessAction
+  | SigninOAuthSuccessAction
+  | SignupSuccessAction
+  | SignoutSuccessAction
+  | SigninEmailFailureAction
+  | SigninOAuthFailureAction
+  | SignupFailureAction
+  | SignoutFailureAction;
