5d0b73de59133e6abddbe488892fabe8c64e9815
awytynck <anneleen.wytynck@ugent.be>
2018-04-18 10:21:35 +0200

    text-to-speech (with errors)

--- app/pages/components/TempSlideTestPage.js
+++ app/pages/components/TempSlideTestPage.js
@@ -9,7 +9,7 @@ import type { State } from 'types/state';
 import contentItems, { contentItemTypes } from 'modules/content-items';
 import type { DenormalizedRootContentItem } from 'modules/content-items';
 import Slide from 'core-components/slides/Slide';
-import color, { TwitterPicker } from 'react-color';
+import { TwitterPicker } from 'react-color';
 
 import Page from '../Page';
 
@@ -46,15 +46,13 @@ const mapStateToProps = (state: State, props: PassedProps): StateProps => {
 
 const PureTempSlideTestPage = (props: Props): React.Node => {
   const { contentItemTreeRootItem } = props;
-  const handleChange = (Color: color): string => {
-    //alert(Color.hex);
-    // this.setState({ background: Color.hex });
-    return Color.hex;
-  };
+  // handleChange = (color, event){
+  //   this.setState({background: color.hex});
+  // };
 
   return (
     <Page>
-      <TwitterPicker onChange={handleChange} />
+      <TwitterPicker onChange={this.handleChange} />
       <Slide contentItemTreeRootItem={contentItemTreeRootItem} />
     </Page>
   );
