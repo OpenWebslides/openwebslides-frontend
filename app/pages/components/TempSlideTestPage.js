fffd8a8c8ef5d6982d10fc14b03e9374f860f004
awytynck <anneleen.wytynck@ugent.be>
2018-04-18 11:44:20 +0200

    text-to-speech test working

--- app/pages/components/TempSlideTestPage.js
+++ app/pages/components/TempSlideTestPage.js
@@ -10,8 +10,8 @@ import contentItems, { contentItemTypes } from 'modules/content-items';
 import type { DenormalizedRootContentItem } from 'modules/content-items';
 import Slide from 'core-components/slides/Slide';
 import color, { TwitterPicker } from 'react-color';
-// import { VoicePlayer } from 'react-voice-components';
-import { Speech } from 'react-speech';
+import VoicePlayer from 'lib/react-voice-components/VoicePlayer';
+// import { Speech } from 'react-speech';
 
 import Page from '../Page';
 
@@ -59,8 +59,9 @@ const PureTempSlideTestPage = (props: Props): React.Node => {
   ); */
   return (
     <Page>
-      <div id="voice" />
-      <Speech text="test" />
+      <div id="voice" >
+        <VoicePlayer play={true} onEnd={(): void => {}} text="test" />
+      </div>
       <TwitterPicker onChange={handleChange} />
       <Slide contentItemTreeRootItem={contentItemTreeRootItem} />
 
