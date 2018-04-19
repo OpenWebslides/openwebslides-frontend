16227504cfa88d0a12d6fd382625a4eda12115f0
Jasper D'haene <jasper.dhaene@hotmail.com>
2018-04-19 16:37:58 +0200

    started linking auth with profile page

--- app/pages/components/ProfilePage.js
+++ app/pages/components/ProfilePage.js
@@ -1,26 +1,55 @@
 // @flow
 
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
 import { translate } from 'react-i18next';
+import type { State } from 'types/state';
 
 import type { CustomTranslatorProps } from 'types/translator';
 
 import users from 'modules/users';
+import authentication from 'modules/authentication';
 
 import Page from '../Page';
 
-const { CURRENT_USER } = users.constants;
+
 const { ProfileCard } = users.components;
+const { getAccount } = authentication.selectors;
+type Account = authentication.model.Account;
 
 type RouteProps = {
   match: Match,
 };
 
+type StateProps = {
+  account: Account,
+};
+
 type Props = CustomTranslatorProps & RouteProps;
 
-const CurrentUserProfile = (): React.Node => {
+const mapStateToProps = (state: State): StateProps => {
+  const account = getAccount(state);
+
+  // TODO: figure out what to do with /profile if no user is logged in
+  /*
+  if (account == null) {
+    throw new Error(`User is not logged in`);
+  }
+  */
+
+  return {
+    account,
+  };
+};
+
+const CurrentUserProfile = (props: StateProps): React.Node => {
+  // TODO: use account from props in this function
+  // Still missing userId in account
+
+  const CURRENT_USER = 'johanjohan';
+
   return (
     <React.Fragment>
       <ProfileCard userId={CURRENT_USER} />
@@ -57,7 +86,7 @@ const PureProfilePage = (props: Props): React.Node => {
   );
 };
 
-const ProfilePage = translate()(PureProfilePage);
+const ProfilePage = connect(mapStateToProps)(translate()(PureProfilePage));
 
 export { PureProfilePage };
 export default ProfilePage;
