48a614baacf0162637759739de96d1ffababb2b6
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-19 13:59:08 +0200

    Export api model types

--- app/lib/api/index.js
+++ app/lib/api/index.js
@@ -4,9 +4,25 @@ import ApiRequest from './ApiRequest';
 
 import * as model from './model/index';
 
+import type {
+  MethodType,
+  Headers,
+  Parameters,
+  RequestConfig,
+  Request,
+} from './model';
+
 const Api = {
   ApiRequest,
   model,
+};
+
+export type {
+  MethodType,
+  Headers,
+  Parameters,
+  RequestConfig,
+  Request,
 };
 
 export default Api;
