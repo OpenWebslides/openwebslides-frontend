e422a5a5ffc3587de813e7011ff80a0f86580b4c
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-23 13:33:58 +0200

    Use environment API_URL in production

--- app/lib/api/constants.js
+++ app/lib/api/constants.js
@@ -1,7 +1,7 @@
 // @flow
 
-// Default API URL
-export const DEFAULT_URL = 'https://openwebslides.ugent.be/api';
+// API base URL
+export const API_URL = (window.config ? window.config.API_URL : 'https://openwebslides.ugent.be/api');
 
 // JSON API media type
 export const MEDIA_TYPE = 'application/vnd.api+json';
