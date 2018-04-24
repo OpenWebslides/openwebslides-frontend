b63174aeeb44f07e80015fc100b7bbe8eec72d05
awytynck <anneleen.wytynck@ugent.be>
2018-04-24 11:44:38 +0200

    Text-to-speech fixing component according to ESLint rules, State only sets after second click on button, component doesn't rerender

--- app/core-components/slides/voicePlayerComp.js
+++ app/core-components/slides/voicePlayerComp.js
@@ -1,48 +1,68 @@
-/*  eslint-disable */
+// @flow
 
 import * as React from 'react';
-import { translate } from 'react-i18next';
 import VoicePlayer from 'lib/react-voice-components/VoicePlayer';
 
+type Props = {
+  initialText: string,
+};
 
-class voicePlayer extends React.Component {
-  constructor(props) {
+type State = {
+  content: string,
+};
+
+class voicePlayer extends React.Component<Props, State> {
+  static defaultProps = {
+    initialText: '',
+  };
+  constructor(props: Props): void {
     super(props);
-
     this.state = {
-      content: '',
+      content: props.initialText,
     };
   }
-    read = () => {
-      const elems = document.getElementsByClassName('inline-markdown');
-      let i:number = 0;
-      let elementen = [];
-      for (i = 0; i < elems.length; i += 1) {
-        elementen.push(elems[i].innerHTML);
-      }
-      let res = elementen.join(" ");
-      // alle elementen worden samengevoegd, werkt
-      this.setState({content: res}, function () {
-        console.log(this.state.content);
-        // voiceplayer wordt niet opnieuw gerenderd.
-      });
 
-    };
-
-    render(): React.Node {
-      return (
-        <div id="player">
-          <button onClick={this.read.bind(this)}>Click me</button>
-          <VoicePlayer
-            play={true}
-            onEnd={(): void => {}}
-            text={this.state.content}
-          />
-        </div>);
+  componentWillReceiveProps = (props: Props): void => {
+    if (this.state.content !== props.initialText) {
+      this.setState({ content: props.initialText });
     }
+  };
+
+  read = (): React.Node => {
+    const elems = document.getElementsByClassName('inline-markdown');
+    let i:number = 0;
+    const elementen = [];
+    for (i = 0; i < elems.length; i += 1) {
+      elementen.push(elems[i].innerHTML);
+    }
+    const res = elementen.join(' ');
+    // alle elementen worden samengevoegd, werkt
+
+    this.setState({ content: res });
+    console.log(this.state.content);
+
+    return (
+      <div id="player">
+        <button onClick={this.read}>Click me</button>
+        <VoicePlayer
+          play={true}
+          onEnd={(): void => {}}
+          text={this.state.content}
+        />
+      </div>);
+  };
+
+  render(): React.Node {
+    return (
+      <div id="player">
+        <button onClick={this.read}>Click me</button>
+        <VoicePlayer
+          play={true}
+          onEnd={(): void => {}}
+          text={this.state.content}
+        />
+      </div>);
+  }
 }
 
-const VoicePlay = translate()(voicePlayer);
-
-export { voicePlayer };
-export default VoicePlay;
+export default voicePlayer;
