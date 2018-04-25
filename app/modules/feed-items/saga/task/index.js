6c4392cb1e96998a8a2f1c057147905281e3e4a3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-25 10:52:18 +0200

    Add feed-items task sagas

--- app/modules/feed-items/saga/task/index.js
+++ app/modules/feed-items/saga/task/index.js
@@ -1,9 +1,14 @@
 // @flow
 
-import { all } from 'redux-saga/effects';
+import { all, takeEvery } from 'redux-saga/effects';
+
+import * as t from '../../actionTypes';
+
+import fetchSaga from './fetch';
 
 const taskSaga = function* (): Generator<*, *, *> {
   yield all([
+    takeEvery(t.FETCH, fetchSaga),
   ]);
 };
 
