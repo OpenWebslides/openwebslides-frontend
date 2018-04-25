d1802445b7d1e0b96b9f6edc7641b800c72fe837
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-25 14:35:05 +0200

    Remodel UserType

--- app/modules/users/model.js
+++ app/modules/users/model.js
@@ -1,23 +1,15 @@
 // @flow
 
 import type { Identifier } from 'types/model';
-import generateRandomString from 'lib/generate-random-string';
-
-// #TODO optimal id length / generation method?
-const ID_LENGTH = 10;
 
 export type UserType = {
   +id: Identifier,
   +firstName: string,
-  +lastName?: ?string,
-  +email: string,
-  +password?: string,
+  +lastName: ?string,
+  +email: ?string,
+  +password: ?string,
 };
 
 export type UsersState = {
   +[userId: Identifier]: UserType,
-};
-
-export const generateId = (): Identifier => {
-  return generateRandomString(ID_LENGTH);
 };
