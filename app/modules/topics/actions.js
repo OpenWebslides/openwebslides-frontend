c74ebfe45b51e49f9567e1b65d24a8f6fa77fde5
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-26 16:38:31 +0200

    Reorganize topics actions

--- app/modules/topics/actions.js
+++ app/modules/topics/actions.js
@@ -9,6 +9,96 @@ import type { Topic } from './model';
 import * as t from './actionTypes';
 import { generateId } from './model';
 
+// Reducer actions
+export const addToState = (
+  userId: Identifier,
+  title: string,
+  description: ?string = null,
+): t.AddToStateAction | t.AddToStateErrorAction => {
+  const newId = generateId();
+  const newTitle = _.trim(title);
+  const newDescription = (description != null) ? _.trim(description) : '';
+
+  if (newTitle === '') {
+    return {
+      type: t.ADD_TO_STATE_ERROR,
+      error: {
+        message: 'Title cannot be empty.',
+      },
+    };
+  }
+
+  return {
+    type: t.ADD_TO_STATE,
+    payload: {
+      id: newId,
+      userId,
+      title: newTitle,
+      description: newDescription,
+      rootContentItemId: 'w4lg2u0p1h', // #TODO stub
+    },
+  };
+};
+
+export const editInState = (
+  id: Identifier,
+  title: ?string = null,
+  description: ?string = null,
+): t.EditInStateAction | t.EditInStateErrorAction => {
+  const newTitle = (title != null) ? _.trim(title) : null;
+  const newDescription = (description != null) ? _.trim(description) : null;
+
+  if (newTitle === '') {
+    return {
+      type: t.EDIT_IN_STATE_ERROR,
+      error: {
+        message: 'Title cannot be empty.',
+      },
+    };
+  }
+
+  if (newTitle === null && newDescription === null) {
+    return {
+      type: t.EDIT_IN_STATE_ERROR,
+      error: {
+        message: 'Action must contain at least one edit.',
+      },
+    };
+  }
+
+  return {
+    type: t.EDIT_IN_STATE,
+    payload: {
+      id,
+      title: newTitle,
+      description: newDescription,
+    },
+  };
+};
+
+export const removeFromState = (
+  id: Identifier,
+): t.RemoveFromStateAction | t.RemoveFromStateErrorAction => {
+  return {
+    type: t.REMOVE_FROM_STATE,
+    payload: {
+      id,
+    },
+  };
+};
+
+export const setItemsInState = (
+  items: Array<Topic>,
+): t.SetItemsInStateAction => {
+  return {
+    type: t.SET_ITEMS_IN_STATE,
+    payload: {
+      items,
+    },
+  };
+};
+
+// Task saga actions
 export const add = (
   userId: Identifier,
   title: string,
@@ -90,72 +180,6 @@ export const remove = (
   };
 };
 
-export const addToState = (
-  userId: Identifier,
-  title: string,
-  description: ?string = null,
-): t.AddToStateAction | t.AddToStateErrorAction => {
-  const newId = generateId();
-  const newTitle = _.trim(title);
-  const newDescription = (description != null) ? _.trim(description) : '';
-
-  if (newTitle === '') {
-    return {
-      type: t.ADD_TO_STATE_ERROR,
-      error: {
-        message: 'Title cannot be empty.',
-      },
-    };
-  }
-
-  return {
-    type: t.ADD_TO_STATE,
-    payload: {
-      id: newId,
-      userId,
-      title: newTitle,
-      description: newDescription,
-      rootContentItemId: 'w4lg2u0p1h', // #TODO stub
-    },
-  };
-};
-
-export const editInState = (
-  id: Identifier,
-  title: ?string = null,
-  description: ?string = null,
-): t.EditInStateAction | t.EditInStateErrorAction => {
-  const newTitle = (title != null) ? _.trim(title) : null;
-  const newDescription = (description != null) ? _.trim(description) : null;
-
-  if (newTitle === '') {
-    return {
-      type: t.EDIT_IN_STATE_ERROR,
-      error: {
-        message: 'Title cannot be empty.',
-      },
-    };
-  }
-
-  if (newTitle === null && newDescription === null) {
-    return {
-      type: t.EDIT_IN_STATE_ERROR,
-      error: {
-        message: 'Action must contain at least one edit.',
-      },
-    };
-  }
-
-  return {
-    type: t.EDIT_IN_STATE,
-    payload: {
-      id,
-      title: newTitle,
-      description: newDescription,
-    },
-  };
-};
-
 export const fetch = (
 ): t.FetchAction | t.FetchErrorAction => {
   return {
@@ -163,28 +187,7 @@ export const fetch = (
   };
 };
 
-export const removeFromState = (
-  id: Identifier,
-): t.RemoveFromStateAction | t.RemoveFromStateErrorAction => {
-  return {
-    type: t.REMOVE_FROM_STATE,
-    payload: {
-      id,
-    },
-  };
-};
-
-export const setItemsInState = (
-  items: Array<Topic>,
-): t.SetItemsInStateAction => {
-  return {
-    type: t.SET_ITEMS_IN_STATE,
-    payload: {
-      items,
-    },
-  };
-};
-
+// API saga actions
 export const apiDeleteTopic = (
   id: Identifier,
 ): t.ApiDeleteTopicAction => {
