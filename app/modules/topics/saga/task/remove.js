216c7066258a28f527c1be956d61e572751a2fb9
Jasper D'haene <jasper.dhaene@hotmail.com>
2018-04-30 15:45:54 +0200

    convert topicCard to class; add modal with local state

--- app/modules/topics/saga/task/remove.js
+++ app/modules/topics/saga/task/remove.js
@@ -1,20 +1,15 @@
 // @flow
 import { put } from 'redux-saga/effects';
-import modals from 'modules/modals';
 import * as t from '../../actionTypes';
 import { apiDeleteTopic } from '../../actions';
-
-const { hideModal } = modals.actions;
 
 // eslint-disable-next-line require-yield
 const removeSaga = function* (action: t.RemoveAction): Generator<*, *, *> {
   const {
     id,
-    modalType,
   } = action.payload;
 
   yield put(apiDeleteTopic(id));
-  yield put(hideModal(modalType));
 };
 
 export default removeSaga;
