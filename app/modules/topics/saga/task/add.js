6ad2868fb80c431efb466dc1dfc8300dc47d695e
Jasper D'haene <jasper.dhaene@hotmail.com>
2018-05-02 13:42:26 +0200

    remove duplicate addToState action

--- app/modules/topics/saga/task/add.js
+++ app/modules/topics/saga/task/add.js
@@ -1,21 +1,18 @@
 // @flow
 import { put } from 'redux-saga/effects';
 import * as t from '../../actionTypes';
-import { apiPostTopic, addToState } from '../../actions';
+import { apiPostTopic } from '../../actions';
 
 // eslint-disable-next-line require-yield
 const addSaga = function* (action: t.AddAction): Generator<*, *, *> {
   const {
-    id,
     userId,
     title,
     description,
-    rootContentItemId,
     history,
   } = action.payload;
 
   yield put(apiPostTopic(userId, title, description));
-  yield put(addToState(id, userId, title, description, rootContentItemId));
 
   history.replace('/library');
 };
