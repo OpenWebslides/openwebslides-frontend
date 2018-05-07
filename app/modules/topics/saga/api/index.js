cb08a7de7c86f3d5e085b4d157386f3ef76fd72b
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-07 13:20:09 +0200

    Add topics API patch saga

--- app/modules/topics/saga/api/index.js
+++ app/modules/topics/saga/api/index.js
@@ -7,6 +7,7 @@ import * as t from '../../actionTypes';
 import { apiDeleteTopicSaga } from './delete';
 import { apiGetAllTopicsByUserIdSaga, apiGetTopicSaga } from './get';
 import { apiPostTopicSaga } from './post';
+import { apiPatchTopicContentSaga } from './patchContent';
 
 const apiSaga = function* (): Generator<*, *, *> {
   yield all([
@@ -14,6 +15,7 @@ const apiSaga = function* (): Generator<*, *, *> {
     takeEvery(t.API_GET_TOPIC, apiGetTopicSaga),
     takeLatest(t.API_GET_ALL_TOPICS_BY_USERID, apiGetAllTopicsByUserIdSaga),
     takeLatest(t.API_POST_TOPIC, apiPostTopicSaga),
+    takeLatest(t.API_PATCH_TOPIC_CONTENT, apiPatchTopicContentSaga),
   ]);
 };
 
---
+++ app/modules/topics/saga/api/patchContent.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { call, select } from 'redux-saga/effects';
+import authentication from 'modules/authentication';
+
+import { TopicsApi } from 'lib/api';
+
+import * as t from '../../actionTypes';
+
+const { getToken } = authentication.selectors;
+
+export const apiPatchTopicContentSaga = function* (
+  action: t.ApiPatchTopicContentAction
+): Generator<*, *, *> {
+  try {
+    const { id, denormalizedRootContentItem } = action.payload;
+    const token = yield select(getToken);
+
+    yield call(TopicsApi.patchContent, id, denormalizedRootContentItem, token);
+  }
+  catch (error) {
+    // TODO
+  }
+};
