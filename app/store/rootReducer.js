b3d8a67bb15fb0e3ea18493df595873632233551
Jasper D'haene <jasper.dhaene@hotmail.com>
2018-05-08 10:14:33 +0200

    added localStorage for authentication module

--- app/store/rootReducer.js
+++ app/store/rootReducer.js
@@ -9,6 +9,7 @@ import { reducer as formReducer } from 'redux-form';
 import modulesReducer from './modulesReducer';
 import errorReducer from './errorReducer';
 
+
 // Don't forget to edit types/state.js when a new state part is added here.
 const rootReducer = combineReducers({
   modules: modulesReducer,
