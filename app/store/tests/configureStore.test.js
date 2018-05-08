61d0985c0b46e08a243165acf657e5795c96201f
Jasper D'haene <jasper.dhaene@hotmail.com>
2018-05-08 10:39:18 +0200

    added jest-localstorage-mock package + fixed configureStore test

--- app/store/tests/configureStore.test.js
+++ app/store/tests/configureStore.test.js
@@ -8,8 +8,17 @@ import rootReducer from '../rootReducer';
 describe(`configureStore`, (): void => {
 
   it(`returns a store created with the rootReducer`, (): void => {
+    const persistedState = {
+      modules: {
+        authentication: {
+          authenticated: false,
+          account: null,
+          token: null,
+        },
+      },
+    };
     const configuredStore = configureStore();
-    const rootReducerStore = createStore(rootReducer);
+    const rootReducerStore = createStore(rootReducer, persistedState);
     expect(configuredStore.getState()).toEqual(rootReducerStore.getState());
   });
 
