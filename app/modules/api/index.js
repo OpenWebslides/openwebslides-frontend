839f1b6e85bdefa6c9623c3c02262260b2e6f4e3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-09 15:30:56 +0200

    Add ApiDimmer

--- app/modules/api/index.js
+++ app/modules/api/index.js
@@ -4,12 +4,14 @@ import * as actions from './actions';
 import * as model from './model';
 import * as selectors from './selectors';
 import * as constants from './constants';
+import components from './components';
 import reducer from './reducer';
 
 import type { ApiState } from './model';
 
 const api = {
   actions,
+  components,
   constants,
   model,
   reducer,
