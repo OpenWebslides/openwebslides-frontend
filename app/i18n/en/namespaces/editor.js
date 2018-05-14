750079904fb5edc3f5b1f8a93a9301856dc8d181
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-14 15:17:52 +0200

    Add ApiDimmer to Editor

--- app/i18n/en/namespaces/editor.js
+++ app/i18n/en/namespaces/editor.js
@@ -4,10 +4,12 @@ const editor = {
   header: 'Topic Editor',
   api: {
     save: {
+      pending: 'Saving...',
       success: 'Topic content saved',
       failure: 'Failed to save topic content',
     },
     load: {
+      pending: 'Loading...',
       success: 'Topic content loaded',
       failure: 'Failed to get topic content',
     },
--- app/modules/topics/components/Editor.js
+++ app/modules/topics/components/Editor.js
@@ -13,6 +13,7 @@ import { Button, Header } from 'semantic-ui-react';
 import ObjectNotFoundError from 'errors/usage-errors/ObjectNotFoundError';
 
 import contentItems from 'modules/content-items';
+import api from 'modules/api';
 
 import type { State } from 'types/state';
 import type { Identifier } from 'types/model';
@@ -20,6 +21,12 @@ import type { Identifier } from 'types/model';
 import type { Topic } from '../model';
 import { getById } from '../selectors';
 import { save, load } from '../actions';
+import {
+  API_GET_TOPIC_CONTENT,
+  API_PATCH_TOPIC_CONTENT,
+} from '../actionTypes';
+
+const { ApiDimmer } = api.components;
 
 type DispatchProps = {
   onSaveButtonClick: (Identifier) => void,
@@ -74,6 +81,9 @@ const PureEditor = (props: Props): React.Node => {
       <Header as="h1">{topic.title}</Header>
 
       <FlashMessages />
+
+      <ApiDimmer request={API_GET_TOPIC_CONTENT}>{t('editor:api.load.pending')}</ApiDimmer>
+      <ApiDimmer request={API_PATCH_TOPIC_CONTENT}>{t('editor:api.save.pending')}</ApiDimmer>
 
       <p>
         <Button primary={true} onClick={() => onSaveButtonClick(topic.id)}>
