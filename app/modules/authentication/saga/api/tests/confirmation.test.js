08826964afe87ecfb308dd5c21518db9eb173065
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-14 10:21:03 +0200

    Fix confirmation test

--- app/modules/authentication/saga/api/tests/confirmation.test.js
+++ app/modules/authentication/saga/api/tests/confirmation.test.js
@@ -3,12 +3,24 @@
 import { expectSaga } from 'redux-saga-test-plan';
 
 import { ConfirmationApi } from 'lib/api';
+import type { Response } from 'lib/api';
 
 import * as t from '../../../actionTypes';
 import { apiPostConfirmationSaga } from '../confirmation';
 
 describe(`confirmation`, (): void => {
-  it(`calls Api.confirm`, (): void => {
+  beforeAll((): void => {
+    // Mock API calls
+    ConfirmationApi.post = (): Promise<Response> => {
+      return Promise.resolve({
+        body: {},
+        token: null,
+        status: 204,
+      });
+    };
+  });
+
+  it(`calls ConfirmationApi.confirm`, (): void => {
     const dummyPostConfirmationAction: t.ApiPostConfirmationAction = {
       type: t.API_POST_CONFIRMATION,
       payload: {
