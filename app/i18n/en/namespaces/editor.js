c17a330974c34cecab37284f3229c05d4e0ad0b3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-14 15:08:29 +0200

    API call request status for Topic#getContent

--- app/i18n/en/namespaces/editor.js
+++ app/i18n/en/namespaces/editor.js
@@ -2,6 +2,12 @@
 
 const editor = {
   header: 'Topic Editor',
+  api: {
+    load: {
+      success: 'Topic content loaded',
+      failure: 'Failed to get topic content',
+    },
+  },
 };
 
 export default editor;
--- app/modules/topics/saga/api/getContent.js
+++ app/modules/topics/saga/api/getContent.js
@@ -1,24 +1,39 @@
 // @flow
 
-import { call, select } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
+import { flashMessage, flashErrorMessage } from 'redux-flash';
+
 import authentication from 'modules/authentication';
 
 import { TopicsApi } from 'lib/api';
+import api from 'modules/api';
 
 import * as t from '../../actionTypes';
 
+const { setTokenInState } = authentication.actions;
 const { getToken } = authentication.selectors;
+
+const { setStatusInState } = api.actions;
+const { statusTypes } = api.model;
 
 export const apiGetTopicContentSaga = function* (
   action: t.ApiGetTopicContentAction,
 ): Generator<*, *, *> {
+  yield put(setStatusInState(t.API_GET_TOPIC_CONTENT, statusTypes.PENDING));
+
   try {
     const { topicId } = action.payload;
     const token = yield select(getToken);
 
-    yield call(TopicsApi.getContent, topicId, token);
+    const response = yield call(TopicsApi.getContent, topicId, token);
+    // TODO: handle response
+
+    yield put(setTokenInState(response.token));
+    yield put(setStatusInState(t.API_GET_TOPIC_CONTENT, statusTypes.SUCCESS));
+    yield put(flashMessage('editor:api.load.success'));
   }
   catch (error) {
-    throw error;
+    yield put(setStatusInState(t.API_GET_TOPIC_CONTENT, statusTypes.FAILURE));
+    yield put(flashErrorMessage('editor:api.load.failure'));
   }
 };
