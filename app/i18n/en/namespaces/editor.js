5e4fd0d45a962d702040f5b7272be634894ee2c1
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-14 15:17:42 +0200

    API call request status for Topic#patchContent

--- app/i18n/en/namespaces/editor.js
+++ app/i18n/en/namespaces/editor.js
@@ -3,6 +3,10 @@
 const editor = {
   header: 'Topic Editor',
   api: {
+    save: {
+      success: 'Topic content saved',
+      failure: 'Failed to save topic content',
+    },
     load: {
       success: 'Topic content loaded',
       failure: 'Failed to get topic content',
--- app/modules/topics/saga/api/patchContent.js
+++ app/modules/topics/saga/api/patchContent.js
@@ -1,24 +1,38 @@
 // @flow
 
-import { call, select } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
+import { flashMessage, flashErrorMessage } from 'redux-flash';
+
 import authentication from 'modules/authentication';
 
 import { TopicsApi } from 'lib/api';
+import api from 'modules/api';
 
 import * as t from '../../actionTypes';
 
+const { setTokenInState } = authentication.actions;
 const { getToken } = authentication.selectors;
+
+const { setStatusInState } = api.actions;
+const { statusTypes } = api.model;
 
 export const apiPatchTopicContentSaga = function* (
   action: t.ApiPatchTopicContentAction,
 ): Generator<*, *, *> {
+  yield put(setStatusInState(t.API_PATCH_TOPIC_CONTENT, statusTypes.PENDING));
+
   try {
     const { topicId, contentItems } = action.payload;
     const token = yield select(getToken);
 
-    yield call(TopicsApi.patchContent, topicId, contentItems, token);
+    const response = yield call(TopicsApi.patchContent, topicId, contentItems, token);
+
+    yield put(setTokenInState(response.token));
+    yield put(setStatusInState(t.API_PATCH_TOPIC_CONTENT, statusTypes.SUCCESS));
+    yield put(flashMessage('editor:api.save.success'));
   }
   catch (error) {
-    // TODO
+    yield put(setStatusInState(t.API_PATCH_TOPIC_CONTENT, statusTypes.FAILURE));
+    yield put(flashErrorMessage('editor:api.save.failure'));
   }
 };
