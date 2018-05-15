8a2cfa64b0a3af5cb4895d21e4a428cc6d8cfd7f
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-15 17:52:57 +0200

    getContent: put reducer action

--- app/modules/topics/saga/api/getContent.js
+++ app/modules/topics/saga/api/getContent.js
@@ -3,7 +3,10 @@
 import { call, put, select } from 'redux-saga/effects';
 import { flashMessage, flashErrorMessage } from 'redux-flash';
 
+import ServerError from 'errors/ServerError';
+
 import authentication from 'modules/authentication';
+import contentItems from 'modules/content-items';
 
 import { TopicsApi } from 'lib/api';
 import api from 'modules/api';
@@ -16,6 +19,9 @@ const { getToken } = authentication.selectors;
 const { setStatusInState } = api.actions;
 const { statusTypes } = api.model;
 
+const { setMultipleInState } = contentItems.actions;
+const { ContentItem } = contentItems.model;
+
 export const apiGetTopicContentSaga = function* (
   action: t.ApiGetTopicContentAction,
 ): Generator<*, *, *> {
@@ -26,13 +32,20 @@ export const apiGetTopicContentSaga = function* (
     const token = yield select(getToken);
 
     const response = yield call(TopicsApi.getContent, topicId, token);
-    // TODO: handle response
+
+    // TODO: validate response
+    const items: Array<ContentItem> = response.body.data.attributes.content;
+    yield put(setMultipleInState(items));
 
     yield put(setTokenInState(response.token));
     yield put(setStatusInState(t.API_GET_TOPIC_CONTENT, statusTypes.SUCCESS));
     yield put(flashMessage('editor:api.load.success'));
   }
   catch (error) {
+    if (!(error instanceof ServerError)) {
+      throw error;
+    }
+
     yield put(setStatusInState(t.API_GET_TOPIC_CONTENT, statusTypes.FAILURE));
     yield put(flashErrorMessage('editor:api.load.failure'));
   }
