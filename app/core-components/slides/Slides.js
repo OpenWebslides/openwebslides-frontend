16577757f522d1cfcc047888244815300a2bf448
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 10:29:29 +0200

    Slides: delegate slide splitting to lib

--- app/core-components/slides/Slides.js
+++ app/core-components/slides/Slides.js
@@ -4,7 +4,12 @@ import * as React from 'react';
 import { translate } from 'react-i18next';
 import type { CustomTranslatorProps } from 'types/translator';
 
-import type { DenormalizedRootContentItem } from 'modules/content-items';
+import type {
+  DenormalizedRootContentItem,
+  ContentItem,
+} from 'modules/content-items';
+
+import split from 'lib/content-item-split';
 
 import Slide from './Slide';
 
@@ -18,9 +23,15 @@ type Props = CustomTranslatorProps & PassedProps;
 const PureSlides = (props: Props): React.Node => {
   const { contentItemTreeRootItem } = props;
 
+  const contentItems: Array<ContentItem> = split(contentItemTreeRootItem);
+
   return (
     <div className="ows_slides">
-      <Slide contentItemTreeRootItem={contentItemTreeRootItem} />
+      {
+        contentItems.map((contentItem) => (
+          <Slide key={contentItem.id} contentItem={contentItem} />
+        ))
+      }
     </div>
   );
 };
---
+++ app/lib/content-item-split/index.js
@@ -0,0 +1,18 @@
+// @flow
+
+import contentItems from 'modules/content-items';
+
+const {
+  DenormalizedRootContentItem,
+  DenormalizedContentItem,
+} = contentItems.model;
+
+/**
+ * split - Splits DenormalizedRootContentItem in a list of DenormalizedContentItem
+ * Also known as automatic slide splitting algorithm
+ */
+const split = (rootContentItem: DenormalizedRootContentItem): Array<DenormalizedContentItem> => {
+  return [rootContentItem];
+};
+
+export default split;
