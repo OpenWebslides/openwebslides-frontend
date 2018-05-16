8cfd720f1015cb23d6f1fd3db7b0dea0d5bdbd49
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 13:08:56 +0200

    Split: handle content items based on type

--- app/lib/content-item-split/index.js
+++ app/lib/content-item-split/index.js
@@ -1,18 +1,40 @@
 // @flow
 
-import contentItems from 'modules/content-items';
-
-const {
-  DenormalizedRootContentItem,
+import type {
   DenormalizedContentItem,
-} = contentItems.model;
+  DenormalizedRootContentItem,
+} from 'modules/content-items';
+
+import { contentItemTypes } from 'modules/content-items';
+
+const recursiveSplit = (
+  contentItem: DenormalizedContentItem,
+): Array<DenormalizedContentItem> => {
+  switch (contentItem.type) {
+    case contentItemTypes.ROOT:
+      console.log(`ROOT: splitting recursively into ${contentItem.childItems.length}`);
+      // ROOT content item: split into childItems and recurse
+      return contentItem.childItems
+        .map((c: DenormalizedContentItem): DenormalizedContentItem => {
+          return recursiveSplit(c);
+        })
+        .reduce((arr, c) => arr.concat(c));
+    case contentItemTypes.HEADING:
+      console.log(`HEADING: adding denormalized content item`);
+      // Add HEADING content item and denormalize
+      return [contentItem];
+    default:
+      console.log(`UNKNOWN: not splitting any further`);
+      return [contentItem];
+  }
+};
 
 /**
  * split - Splits DenormalizedRootContentItem in a list of DenormalizedContentItem
  * Also known as automatic slide splitting algorithm
  */
 const split = (rootContentItem: DenormalizedRootContentItem): Array<DenormalizedContentItem> => {
-  return rootContentItem.childItems;
+  return recursiveSplit(rootContentItem);
 };
 
 export default split;
