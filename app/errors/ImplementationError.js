2e7d9f63bac9483e6a4df8e8096bd26a5f398896
Dromin <contact@dromin.be>
2018-05-16 22:14:47 +0200

    Applied temporary workaround for issue where `jest --coverage` would incorrectly fail tests containing `expect(...).toThrow(CustomError)` because of a bug in instanbul / babel-plugin-transform-builtin-extend. See https://github.com/istanbuljs/babel-plugin-istanbul/issues/143

--- app/errors/ImplementationError.js
+++ app/errors/ImplementationError.js
@@ -10,6 +10,14 @@ class ImplementationError extends CustomError {
   // and should not be shown to the user.
   constructor(message: string, isTranslatable: boolean = false): void {
     super(message, isTranslatable);
+
+    // Temporary workaround for https://github.com/istanbuljs/babel-plugin-istanbul/issues/143 #TODO
+    /* eslint-disable no-proto */
+    // $FlowFixMe
+    this.constructor = ImplementationError;
+    // $FlowFixMe
+    this.__proto__ = ImplementationError.prototype;
+    /* eslint-enable */
   }
 }
 
