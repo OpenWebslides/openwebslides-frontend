f6a67fb503403136aeba470a60324948e05d25b7
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-17 11:08:54 +0200

    Fetch topic on topic content load

--- app/modules/topics/saga/task/load.js
+++ app/modules/topics/saga/task/load.js
@@ -2,13 +2,14 @@
 
 import { put } from 'redux-saga/effects';
 import * as t from '../../actionTypes';
-import { apiGetTopicContent } from '../../actions';
+import { apiGetTopic, apiGetTopicContent } from '../../actions';
 
 const loadSaga = function* (action: t.LoadContentAction): Generator<*, *, *> {
   const {
     id,
   } = action.payload;
 
+  yield put(apiGetTopic(id));
   yield put(apiGetTopicContent(id));
 };
 
