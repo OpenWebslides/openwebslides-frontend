57577647bfd181c79f173844f0a9e3a7c3232e55
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-23 13:59:13 +0200

    Move API configuration out of lib/ into config/ (closes #97)

--- app/lib/api/constants.js
+++ app/lib/api/constants.js
@@ -1,7 +1,4 @@
 // @flow
 
-// API base URL
-export const API_URL = (window.config ? window.config.API_URL : 'http://owsdev.ugent.be/api');
-
 // JSON API media type
 export const MEDIA_TYPE = 'application/vnd.api+json';
