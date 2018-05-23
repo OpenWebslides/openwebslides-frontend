63b2d04c75497b8025ffe73b9cf74a750b499d57
Dromin <contact@dromin.be>
2018-05-23 23:19:31 +0200

    Added GenerateRandomStringPage to help generate test data.

---
+++ app/pages/components/GenerateRandomStringPage.js
@@ -0,0 +1,43 @@
+// @flow
+
+import * as React from 'react';
+import type { Match } from 'react-router-dom';
+import generateRandomString from 'lib/generate-random-string';
+
+import Page from '../Page';
+
+type RouteProps = {
+  match: Match,
+};
+
+type Props = RouteProps & {};
+
+const PureGenerateRandomStringPage = (props: Props): React.Node => {
+  const { match } = props;
+  const lengthParam = parseInt(match.params.length, 10);
+  const randomStringLength = lengthParam || 20;
+  return (
+    <Page>
+      <h1>
+        Randomly generated strings of length { randomStringLength }:
+      </h1>
+      <div style={{ fontFamily: 'monospace' }}>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+        <p>{ generateRandomString(randomStringLength) }</p>
+      </div>
+    </Page>
+  );
+};
+
+const GenerateRandomStringPage = PureGenerateRandomStringPage;
+
+export { PureGenerateRandomStringPage };
+export default GenerateRandomStringPage;
