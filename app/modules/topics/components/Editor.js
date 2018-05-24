f187d6aab6440b049ab121a3b2e05089f72a4186
Dromin <contact@dromin.be>
2018-05-24 11:42:06 +0200

    Undid temporary change where slide was displayed alongside topic editor.

--- app/modules/topics/components/Editor.js
+++ app/modules/topics/components/Editor.js
@@ -4,14 +4,13 @@ import * as React from 'react';
 import { translate } from 'react-i18next';
 import type { CustomTranslatorProps } from 'types/translator';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { Header } from 'semantic-ui-react';
 
 import ObjectNotFoundError from 'errors/usage-errors/ObjectNotFoundError';
 
-import contentItems, { contentItemTypes } from 'modules/content-items';
-import type { DenormalizedRootContentItem } from 'modules/content-items';
-import Slide from 'core-components/slides/Slide';
+import contentItems from 'modules/content-items';
 
 import type { State } from 'types/state';
 import type { Identifier } from 'types/model';
@@ -25,7 +24,6 @@ type PassedProps = {
 
 type StateProps = {
   topic: Topic,
-  contentItemTreeRootItem: DenormalizedRootContentItem,
 };
 
 type Props = CustomTranslatorProps & PassedProps & StateProps;
@@ -37,46 +35,22 @@ const mapStateToProps = (state: State, props: PassedProps): StateProps => {
     throw new ObjectNotFoundError('topics:topic', props.topicId);
   }
 
-  const contentItemTreeRootItemId = topic.rootContentItemId;
-  const contentItemTreeRootItem = contentItems.selectors.getDenormalizedById(
-    state,
-    { id: contentItemTreeRootItemId },
-  );
-
-  if (contentItemTreeRootItem == null) {
-    throw new Error(`ContentItem with id "${contentItemTreeRootItemId}" could not be found.`);
-  }
-  else if (contentItemTreeRootItem.type !== contentItemTypes.ROOT) {
-    throw new Error('Not a ROOT contentItem.');
-  }
-
   return {
     topic,
-    contentItemTreeRootItem,
   };
 };
 
 const ContentItemEditableDisplay = contentItems.components.EditableDisplay;
 
 const PureEditor = (props: Props): React.Node => {
-  const { topic, contentItemTreeRootItem } = props;
+  const { topic } = props;
 
   return (
     <div>
       <Header as="h1">{topic.title}</Header>
-      <div style={{ display: 'table', width: '100%' }}>
-        <div style={{ display: 'table-cell', paddingRight: '1em', verticalAlign: 'top' }}>
-          { /* $FlowFixMe See: https://github.com/facebook/flow/issues/4644 */ }
-          <ContentItemEditableDisplay contentItemId={topic.rootContentItemId} />
-        </div>
-        <div style={{ display: 'table-cell', paddingLeft: '1em', verticalAlign: 'top' }}>
-          <div style={{ marginTop: '1em' }}>
-            <div style={{ fontSize: '0.7em' }}>
-              <Slide contentItemTreeRootItem={contentItemTreeRootItem} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Link to="/tempslidetest">Temp slide test page</Link>
+      { /* $FlowFixMe See: https://github.com/facebook/flow/issues/4644 */ }
+      <ContentItemEditableDisplay contentItemId={topic.rootContentItemId} />
     </div>
   );
 };
