e24b8498f37a39144da79c412d65175e3b365872
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-06-02 11:09:53 +0200

    Revert Slide to render only RootContentItem

--- app/core-components/slides/Slide.js
+++ app/core-components/slides/Slide.js
@@ -5,11 +5,11 @@ import { translate } from 'react-i18next';
 import type { CustomTranslatorProps } from 'types/translator';
 
 import contentItems from 'modules/content-items';
-import type { DenormalizedContentItem } from 'modules/content-items';
+import type { DenormalizedRootContentItem } from 'modules/content-items';
 
 type PassedProps = {
-  // A denormalized content item containing the content to be displayed on this slide.
-  contentItem: DenormalizedContentItem,
+  // A denormalized ROOT content item containing the content to be displayed on this slide.
+  contentItem: DenormalizedRootContentItem,
   // The heading level of the top level headings on the slide. Defaults to 1.
   rootHeadingLevel: number,
 };
