4bac780ec2a1b8a0c65b923a4a768e945ae9c659
Dromin <contact@dromin.be>
2018-06-12 12:43:11 +0200

    Changes to the way descendant items are fetched in topics saveSaga: - Removed content-items/lib/get-all-descendants, since its functionality is covered by content-items/lib/find/getAllDescendantItems - Renamed getAllDescendantsById selector into getSelfAndAllDescendantsById to better describe its functionality; refactored it to use the find lib - Made getSelfAndAllDescendantsById a cached selector using re-reselect, since it takes the id as an argument - Refactored topics saveSaga to use getSelfAndAllDescendantsById selector

--- app/modules/topics/saga/task/save.js
+++ app/modules/topics/saga/task/save.js
@@ -8,17 +8,16 @@ import { apiPatchContent } from '../../actions';
 import { getById } from '../../selectors';
 import type { Topic } from '../../model';
 
-const { getAllDescendantsById } = contentItems.selectors;
-
 const saveSaga = function* (action: t.SaveContentAction): Generator<*, *, *> {
   const {
     id,
   } = action.payload;
 
   const topic: Topic = yield select(getById, { id });
-  const contentItemDescendants = yield select(getAllDescendantsById, {
-    id: topic.rootContentItemId,
-  });
+  const contentItemDescendants = yield select(
+    contentItems.selectors.getSelfAndAllDescendantsById,
+    { id: topic.rootContentItemId },
+  );
 
   yield put(apiPatchContent(id, contentItemDescendants));
 };
