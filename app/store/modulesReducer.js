882cc2819572400fd18cd813789ccd91c2ae72e1
Dromin <contact@dromin.be>
2018-07-15 09:41:38 +0200

    Ordered types/state / modulesReducer / rootSaga alphabetically.

--- app/store/modulesReducer.js
+++ app/store/modulesReducer.js
@@ -2,25 +2,25 @@
 
 import { combineReducers } from 'redux';
 
-import topics from 'modules/topics';
+import apiRequestsStatus from 'modules/apiRequestsStatus';
+import authentication from 'modules/authentication';
 import contentItems from 'modules/contentItems';
 import feed from 'modules/feed';
-import sidebars from 'modules/sidebars';
-import users from 'modules/users';
-import authentication from 'modules/authentication';
-import apiRequestsStatus from 'modules/apiRequestsStatus';
 import history from 'modules/history';
+import sidebars from 'modules/sidebars';
+import topics from 'modules/topics';
+import users from 'modules/users';
 
 // Don't forget to edit types/state.js when a new state part is added here.
 const modulesReducer = combineReducers({
-  topics: topics.reducer,
-  feed: feed.reducer,
-  sidebars: sidebars.reducer,
-  users: users.reducer,
-  contentItems: contentItems.reducer,
-  authentication: authentication.reducer,
   apiRequestsStatus: apiRequestsStatus.reducer,
+  authentication: authentication.reducer,
+  contentItems: contentItems.reducer,
+  feed: feed.reducer,
   history: history.reducer,
+  sidebars: sidebars.reducer,
+  topics: topics.reducer,
+  users: users.reducer,
 });
 
 export default modulesReducer;
