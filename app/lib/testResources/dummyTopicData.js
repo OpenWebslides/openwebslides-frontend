f7308aad53d767cd58babb4b1a9810f90002823f
Dromin <contact@dromin.be>
2018-07-23 00:46:51 +0200

    Removed dependency of dummyTopicData on dummyContentItemData.

--- app/lib/testResources/dummyTopicData.js
+++ app/lib/testResources/dummyTopicData.js
@@ -2,15 +2,13 @@
 
 import topics from 'modules/topics';
 
-import * as dummyContentItemData from './dummyContentItemData';
-
 const { model } = topics;
 
 export const topic: $Exact<model.Topic> = {
   id: 'dummyTopic',
   title: 'Dummy Topic',
   description: 'This is a dummy topic for testing purposes',
-  rootContentItemId: dummyContentItemData.rootContentItem.id,
+  rootContentItemId: 'dummyRootContentItemId',
   userId: 'dummyUserId',
 };
 
@@ -18,6 +16,6 @@ export const topic2: $Exact<model.Topic> = {
   id: 'dummyTopic2',
   title: 'Anoterh dummy Topic',
   description: null,
-  rootContentItemId: dummyContentItemData.rootContentItem2.id,
+  rootContentItemId: 'dummyRootContentItemId',
   userId: 'dummyUserId',
 };
