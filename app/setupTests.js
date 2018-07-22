3861e09b3ac57416110e9cdfc85b74f27415e0f5
Dromin <contact@dromin.be>
2018-07-22 23:40:56 +0200

    Removed lib/localStorage.

--- app/setupTests.js
+++ app/setupTests.js
@@ -9,26 +9,3 @@ configure({ adapter: new Adapter() });
 
 // Mock fetch API
 global.fetch = require('jest-fetch-mock');
-
-/* commenting this out because it's not clear what the use is #TODO
-// Mock localStorage
-const localStorageMock = (): * => {
-  let store: { [key: string]: string } = {};
-  return {
-    getItem(key: string): string {
-      return store[key];
-    },
-    setItem(key: string, value: string): void {
-      store[key] = value.toString();
-    },
-    clear(): void {
-      store = {};
-    },
-    removeItem(key: string): void {
-      delete store[key];
-    },
-  };
-};
-
-Object.defineProperty(window, 'localStorage', { value: localStorageMock() });
-*/
