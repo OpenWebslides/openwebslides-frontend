50f85ce145ea78adf20c24c47dd2af837fc57692
Dromin <contact@dromin.be>
2018-07-24 12:59:07 +0200

    Added DummyProviders component to lib/testResources in order to help simplify tests that use enzyme mount() (instead of shallow()).

--- app/lib/testResources/index.js
+++ app/lib/testResources/index.js
@@ -4,10 +4,12 @@ import * as dummyContentItemData from './dummyContentItemData';
 import * as dummyTopicData from './dummyTopicData';
 import * as dummyUserData from './dummyUserData';
 import * as dummyProviderProps from './dummyProviderProps';
+import DummyProviders from './DummyProviders';
 
 export {
   dummyContentItemData,
   dummyTopicData,
   dummyUserData,
   dummyProviderProps,
+  DummyProviders,
 };
