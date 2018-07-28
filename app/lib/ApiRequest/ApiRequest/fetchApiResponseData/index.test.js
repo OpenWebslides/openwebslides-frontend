413616a521d3033eeb832e589126b43c38b25fff
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-07-28 08:52:22 +0200

    Fix SyntaxError: Unexpected JSON token on signout

--- app/lib/ApiRequest/ApiRequest/fetchApiResponseData/index.test.js
+++ app/lib/ApiRequest/ApiRequest/fetchApiResponseData/index.test.js
@@ -29,10 +29,22 @@ describe(`fetchApiResponseData`, (): void => {
       });
   });
 
+  it(`returns a ResponseData object with an empty body on empty response body`, async (): Promise<mixed> => {
+    fetch.mockResponseOnce('', { status: 204 });
+
+    await expect(fetchApiResponseData('', {}))
+      .resolves
+      .toEqual({
+        body: {},
+        status: 204,
+        token: null,
+      });
+  });
+
   it(`returns a ResponseData object with the correct token`, async (): Promise<mixed> => {
     const dummyToken = 'foobarToken';
     fetch.mockResponseOnce(
-      null,
+      '',
       { status: 200, headers: { Authorization: `Bearer ${dummyToken}` } },
     );
 
@@ -46,7 +58,7 @@ describe(`fetchApiResponseData`, (): void => {
   });
 
   it(`throws an Http401UnauthorizedError, when the response contains a 401 status code`, async (): Promise<mixed> => {
-    fetch.mockResponseOnce(null, { status: 401 });
+    fetch.mockResponseOnce('', { status: 401 });
 
     await expect(fetchApiResponseData('', {}))
       .rejects
@@ -54,7 +66,7 @@ describe(`fetchApiResponseData`, (): void => {
   });
 
   it(`throws an Http403ForbiddenError, when the response contains a 403 status code`, async (): Promise<mixed> => {
-    fetch.mockResponseOnce(null, { status: 403 });
+    fetch.mockResponseOnce('', { status: 403 });
 
     await expect(fetchApiResponseData('', {}))
       .rejects
@@ -72,7 +84,7 @@ describe(`fetchApiResponseData`, (): void => {
 
   it(`throws an Http5xxServerError, when the response contains a 5xx status code`, async (): Promise<mixed> => {
     const dummyErrorText = `Service unavailable`;
-    fetch.mockResponseOnce(null, { status: 503, statusText: dummyErrorText });
+    fetch.mockResponseOnce('', { status: 503, statusText: dummyErrorText });
 
     await expect(fetchApiResponseData('', {}))
       .rejects
@@ -81,7 +93,7 @@ describe(`fetchApiResponseData`, (): void => {
 
   it(`throws an UnexpectedHttpStatusError, when the response contains a status code that is not otherwise handled`, async (): Promise<mixed> => {
     const dummyErrorText = `I'm a teapot`;
-    fetch.mockResponseOnce(null, { status: 418, statusText: dummyErrorText });
+    fetch.mockResponseOnce('', { status: 418, statusText: dummyErrorText });
 
     await expect(fetchApiResponseData('', {}))
       .rejects
