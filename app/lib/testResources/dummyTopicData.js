a0a6f7a1ba0dc9e1c52d18dfc3848d9cff72ca88
Dromin <contact@dromin.be>
2018-07-30 13:49:26 +0200

    Added additional dummy topics to lib/testResources.

--- app/lib/testResources/dummyTopicData.js
+++ app/lib/testResources/dummyTopicData.js
@@ -17,3 +17,19 @@ export const topic2: topics.model.Topic = {
   rootContentItemId: 'dummyRootContentItemId',
   userId: 'dummyUserId',
 };
+
+export const topic3: topics.model.Topic = {
+  id: 'dummyTopic3',
+  title: 'This topic is a dummy',
+  description: null,
+  rootContentItemId: 'dummyRootContentItemId',
+  userId: 'dummyUserId',
+};
+
+export const topic4: topics.model.Topic = {
+  id: 'dummyTopic4',
+  title: 'Lorme ipsum dolor sit amet',
+  description: 'Lorem ipsum descriptium',
+  rootContentItemId: 'dummyRootContentItemId',
+  userId: 'dummyUserId',
+};
