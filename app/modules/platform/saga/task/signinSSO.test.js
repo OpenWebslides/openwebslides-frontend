210142683f7383daa13c27ff63d08c11d354b1ad
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-10 16:59:01 +0200

    Add signinSSO saga

---
+++ app/modules/platform/saga/task/signinSSO.test.js
@@ -0,0 +1,30 @@
+// @flow
+
+import { expectSaga } from 'redux-saga-test-plan';
+
+import users from 'modules/users';
+
+import actions from '../../actions';
+
+import { sagas } from '..';
+
+describe(`signinSSO`, (): void => {
+
+  let dummyApiToken: string;
+  let dummyUserId: string;
+
+  beforeEach((): void => {
+    dummyApiToken = 'foobarToken';
+    dummyUserId = 'foobarUserId';
+  });
+
+  it(`puts a fetch and a setUserAuthInState action`, (): void => {
+    const dummyAction = actions.signinSSO(dummyApiToken, dummyUserId);
+
+    return expectSaga(sagas.signinSSO, dummyAction)
+      .put(users.actions.fetch(dummyUserId))
+      .put(actions.setUserAuthInState({ apiToken: dummyApiToken, userId: dummyUserId }))
+      .run();
+  });
+
+});
