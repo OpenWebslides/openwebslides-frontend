b51ae3218e69d07d23ad318a218a0c2d9936e7bc
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-22 10:22:27 +0200

    Bump API version to 4.0.0

--- app/config/api.js
+++ app/config/api.js
@@ -4,4 +4,4 @@
 export const API_URL = (window.config ? window.config.API_URL : 'http://owsdev.ugent.be/api');
 
 // API version
-export const API_VERSION = '3.0.0';
+export const API_VERSION = '4.0.0';
