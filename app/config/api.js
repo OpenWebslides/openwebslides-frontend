ae510d492ddab55f1e609fe66af28d3c40a242c3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-29 10:53:09 +0200

    Revert API endpoint

--- app/config/api.js
+++ app/config/api.js
@@ -1,7 +1,7 @@
 // @flow
 
 // API base URL
-export const API_URL = (window.config ? window.config.API_URL : 'http://localhost:3000/api');
+export const API_URL = (window.config ? window.config.API_URL : 'http://owsdev.ugent.be/api');
 
 // API version
 export const API_VERSION = '4.0.0';
