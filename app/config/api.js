215517232de0c76fa59f387c31f233040a00cf2a
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-29 11:29:05 +0200

    Allow specifying API_URL by env variable

--- app/config/api.js
+++ app/config/api.js
@@ -1,7 +1,7 @@
 // @flow
 
 // API base URL
-export const API_URL = (window.config ? window.config.API_URL : 'http://owsdev.ugent.be/api');
+export const API_URL = window.API_URL || 'http://owsdev.ugent.be/api';
 
 // API version
 export const API_VERSION = '4.0.0';
