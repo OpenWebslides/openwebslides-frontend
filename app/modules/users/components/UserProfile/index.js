60da1e677ab2109d6de854697508ef4801fc8f73
Dromin <contact@dromin.be>
2018-09-04 11:33:09 +0200

    Fixed new flow errors after flow-bin update.

--- app/modules/users/components/UserProfile/index.js
+++ app/modules/users/components/UserProfile/index.js
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { translate, type TranslatorProps } from 'react-i18next';
 import { connect } from 'react-redux';
+import { type Dispatch } from 'redux';
 import { Link } from 'react-router-dom';
 import { Button, Divider, Item } from 'semantic-ui-react';
 
