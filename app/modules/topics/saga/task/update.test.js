f4318cc31ff2a1eed6f9968dffa19c97bb868cba
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-06 09:38:41 +0200

    Fix failing test by providing a putAndReturn

--- app/modules/topics/saga/task/update.test.js
+++ app/modules/topics/saga/task/update.test.js
@@ -1,10 +1,13 @@
 // @flow
 
 import { expectSaga } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
+import { dynamic } from 'redux-saga-test-plan/providers';
 
 import asyncRequests from 'modules/asyncRequests';
 
 import actions from '../../actions';
+import * as a from '../../actionTypes';
 
 import { sagas } from '..';
 
@@ -24,8 +27,13 @@ describe(`update`, (): void => {
     const dummyAction = actions.update(dummyId, dummyTitle, dummyDescription);
 
     return expectSaga(sagas.update, dummyAction)
+      .provide([
+        [matchers.call.fn(asyncRequests.lib.putAndReturn), dynamic(({ args: [action] }: any, next: any): any => {
+          return (action.type === a.API_PATCH) ? null : next();
+        })],
+      ])
       .call(asyncRequests.lib.putAndReturn, actions.apiPatch(dummyId, dummyTitle, dummyDescription))
-      .call(asyncRequests.lib.putAndReturn, actions.fetch(dummyId))
+      .put(actions.fetch(dummyId))
       .run();
   });
 
