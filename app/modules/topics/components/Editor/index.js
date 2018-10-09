3c6c88935e6e3499354d5586f9bf052e4850ada6
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-09 11:37:29 +0200

    Move editor back/cancel button to the left

--- app/modules/topics/components/Editor/index.js
+++ app/modules/topics/components/Editor/index.js
@@ -5,7 +5,7 @@ import { withNamespaces, type TranslatorProps } from 'react-i18next';
 import { connect } from 'react-redux';
 import { Link, Prompt } from 'react-router-dom';
 import { type Dispatch } from 'redux';
-import { Button, Header, Icon } from 'semantic-ui-react';
+import { Button, Header, Icon, Grid } from 'semantic-ui-react';
 
 import { type ModulesAction } from 'types/redux';
 import { USER_PROFILE_ROUTE } from 'config/routes';
@@ -63,32 +63,41 @@ class PureEditor extends React.Component<Props> {
           message={t('topics:modals.unsavedChanges.message')}
         />
 
-        <div style={{ overflow: 'hidden' }}>
-          <Header floated="left" as="h1">{topic.title}</Header>
-          <Button
-            floated="right"
-            disabled={!topic.isDirty}
-            primary={true}
-            icon={true}
-            labelPosition="left"
-            onClick={this.handleSaveButtonClick}
-            data-test-id="topic-editor-save-button"
-          >
-            <Icon name="save" />
-            {t('common:button.save')}
-          </Button>
-          <Button
-            floated="right"
-            icon={true}
-            labelPosition="left"
-            data-test-id="topic-editor-cancel-button"
-            as={Link}
-            to={USER_PROFILE_ROUTE}
-          >
-            <Icon name="cancel" />
-            {t('common:button.cancel')}
-          </Button>
-        </div>
+        <Grid>
+          <Grid.Row columns="equal">
+            <Grid.Column width={1}>
+              <Button
+                circular={true}
+                basic={true}
+                floated="left"
+                icon={true}
+                data-test-id="topic-editor-back-button"
+                as={Link}
+                to={USER_PROFILE_ROUTE}
+              >
+                <Icon name="arrow left" />
+              </Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Header floated="left" as="h1">{topic.title}</Header>
+            </Grid.Column>
+            <Grid.Column width={2}>
+              <Button
+                floated="right"
+                disabled={!topic.isDirty}
+                primary={true}
+                icon={true}
+                labelPosition="left"
+                onClick={this.handleSaveButtonClick}
+                data-test-id="topic-editor-save-button"
+              >
+                <Icon name="save" />
+                {t('common:button.save')}
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
         <ContentItemEditableDisplay contentItemId={topic.rootContentItemId} />
       </div>
     );
