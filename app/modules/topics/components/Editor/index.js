2eb9e41c8af72b980ce3b6eaa17ff28331bceb8b
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-21 09:51:31 +0100

    Remove editor back button

--- app/modules/topics/components/Editor/index.js
+++ app/modules/topics/components/Editor/index.js
@@ -3,12 +3,11 @@
 import * as React from 'react';
 import { withNamespaces, type TranslatorProps } from 'react-i18next';
 import { connect } from 'react-redux';
-import { Link, Prompt } from 'react-router-dom';
+import { Prompt } from 'react-router-dom';
 import { type Dispatch } from 'redux';
-import { Button, Header, Icon, Grid } from 'semantic-ui-react';
+import { Button, Header, Icon } from 'semantic-ui-react';
 
 import { type AppState, type ModulesAction } from 'types/redux';
-import { USER_PROFILE_ROUTE } from 'config/routes';
 import FetchWrapper from 'components/FetchWrapper';
 import contentItems from 'modules/contentItems';
 
@@ -59,7 +58,7 @@ const mapDispatchToProps = (
     },
     discard: (): void => {
       dispatch(actions.discard(topicId));
-    }
+    },
   };
 };
 
@@ -99,43 +98,24 @@ class PureEditor extends React.Component<Props> {
           message={t('topics:modals.unsavedChanges.message')}
         />
 
-        <Grid>
-          <Grid.Row columns="equal">
-            <Grid.Column width={1}>
-              <Button
-                circular={true}
-                basic={true}
-                floated="left"
-                icon={true}
-                data-test-id="topic-editor-back-button"
-                as={Link}
-                to={USER_PROFILE_ROUTE}
-              >
-                <Icon name="arrow left" />
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Header floated="left" as="h1" data-test-id="topic-editor-title">
-                {topic.title}
-                {(topic.isDirty ? '*' : '')}
-              </Header>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <Button
-                floated="right"
-                disabled={!topic.isDirty}
-                primary={true}
-                icon={true}
-                labelPosition="left"
-                onClick={this.handleSaveButtonClick}
-                data-test-id="topic-editor-save-button"
-              >
-                <Icon name="save" />
-                {t('common:button.save')}
-              </Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <div style={{ overflow: 'hidden' }}>
+          <Button
+            floated="right"
+            disabled={!topic.isDirty}
+            primary={true}
+            icon={true}
+            labelPosition="left"
+            onClick={this.handleSaveButtonClick}
+            data-test-id="topic-editor-save-button"
+          >
+            <Icon name="save" />
+            {t('common:button.save')}
+          </Button>
+          <Header floated="left" as="h1" data-test-id="topic-editor-title">
+            {topic.title}
+            {(topic.isDirty ? '*' : '')}
+          </Header>
+        </div>
 
         <ContentItemEditableDisplay
           contentItemId={topic.rootContentItemId}
