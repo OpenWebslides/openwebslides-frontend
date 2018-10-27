05c33cdcb688f9e8badfe21b72d1b31c018d39cf
Dromin <contact@dromin.be>
2018-10-27 12:30:35 +0200

    Updated PageWrapper to clear all flash messages when transitioning from one page to another.

--- app/components/PageWrapper/index.test.js
+++ app/components/PageWrapper/index.test.js
@@ -1,20 +1,41 @@
 // @flow
 
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import { clearMessages } from 'redux-flash';
 
-import { PurePageWrapper } from '.';
+import { DummyProviders } from 'lib/testResources';
+
+import PageWrapper, { PurePageWrapper } from '.';
 
 describe(`PageWrapper`, (): void => {
 
+  let dummyDispatch: any;
+
+  beforeEach((): void => {
+    dummyDispatch = jest.fn();
+  });
+
   it(`renders without errors`, (): void => {
     const enzymeWrapper = shallow(
-      <PurePageWrapper>
+      <PurePageWrapper onClearFlashMessages={jest.fn()}>
         <h1>Lorem ipsum</h1>
         <p>Lorem ipsum dolor sit amet.</p>
       </PurePageWrapper>,
     );
     expect(enzymeWrapper.isEmptyRender()).toBe(false);
+  });
+
+  it(`clears all flash messages on page load`, (): void => {
+    mount(
+      <DummyProviders dummyDispatch={dummyDispatch}>
+        <PageWrapper>
+          <h1>Lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </PageWrapper>
+      </DummyProviders>,
+    );
+    expect(dummyDispatch).toHaveBeenCalledWith(clearMessages());
   });
 
 });
