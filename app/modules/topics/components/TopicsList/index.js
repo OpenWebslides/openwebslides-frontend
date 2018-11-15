777fd27468fb47ad7b4c75cf4cc7881e2de7ef32
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-11-15 14:20:17 +0000

    Move create topic button

--- app/modules/topics/components/TopicsList/index.js
+++ app/modules/topics/components/TopicsList/index.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { withNamespaces, type TranslatorProps } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { Header, Grid, Button, Card, Icon } from 'semantic-ui-react';
+import { Header, Grid, Button, Card } from 'semantic-ui-react';
 
 import { TOPIC_NEW_ROUTE } from 'config/routes';
 
@@ -17,49 +17,42 @@ type PassedProps = {|
 
 type Props = {| ...TranslatorProps, ...PassedProps |};
 
-class PureTopicsList extends React.Component<Props> {
-  renderNewTopicButton = (): React.Node => {
-    const { t, isCurrentUser } = this.props;
-    return (isCurrentUser === false) ? null : (
-      <Card>
-        <Button
-          as={Link}
-          to={TOPIC_NEW_ROUTE}
-          primary={true}
-          size="big"
-          className="topics-list__add-button"
-          data-test-id="topics-list-add-button"
-        >
-          {t('global:title.createNewTopic')}
-        </Button>
-      </Card>
-    );
-  };
-
-  render(): React.Node {
-    const { t, topicIds, isCurrentUser, onRemoveTopic } = this.props;
-    return (
-      <Grid padded={true}>
-        <Grid.Row>
-          <Header>{t('global:title.library')}</Header>
-        </Grid.Row>
-        <Grid.Row>
-          <Card.Group itemsPerRow={3} doubling={true} stackable={true}>
-            {this.renderNewTopicButton()}
-            {[...topicIds].reverse().map((topicId) => (
-              <TopicCard
-                key={topicId}
-                topicId={topicId}
-                isCurrentUser={isCurrentUser}
-                onRemoveTopic={onRemoveTopic}
-              />
-            ))}
-          </Card.Group>
-        </Grid.Row>
-      </Grid>
-    );
-  }
-}
+const PureTopicsList = (props: Props): React.Node => {
+  const { t, topicIds, isCurrentUser, onRemoveTopic } = props;
+  return (
+    <Grid padded={true}>
+      <Grid.Row>
+        <div style={{ width: '100%' }}>
+          {(isCurrentUser ? (
+            <Button
+              as={Link}
+              to={TOPIC_NEW_ROUTE}
+              primary={true}
+              floated="right"
+              className="topics-list__add-button"
+              data-test-id="topics-list-add-button"
+            >
+              {t('global:title.createNewTopic')}
+            </Button>
+          ) : null)}
+          <Header as="h3" floated="left">{t('global:title.library')}</Header>
+        </div>
+      </Grid.Row>
+      <Grid.Row>
+        <Card.Group itemsPerRow={3} doubling={true} stackable={true}>
+          {[...topicIds].reverse().map((topicId) => (
+            <TopicCard
+              key={topicId}
+              topicId={topicId}
+              isCurrentUser={isCurrentUser}
+              onRemoveTopic={onRemoveTopic}
+            />
+          ))}
+        </Card.Group>
+      </Grid.Row>
+    </Grid>
+  );
+};
 
 const TopicsList = withNamespaces()(PureTopicsList);
 
