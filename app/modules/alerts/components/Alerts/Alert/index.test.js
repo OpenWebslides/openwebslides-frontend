4173395fdeaa9296190aba0e73a5d42c21318dce
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-11-16 15:03:41 +0000

    Add test for Alert: rendering PullRequestAlert

--- app/modules/alerts/components/Alerts/Alert/index.test.js
+++ app/modules/alerts/components/Alerts/Alert/index.test.js
@@ -16,6 +16,7 @@ describe(`Alert`, (): void => {
   let dummyTopic: topics.model.Topic;
   let dummyUser: users.model.User;
   let dummyUpdateAlert: m.UpdateAlert;
+  let dummyPullRequestAlert: m.PullRequestAlert;
   let dummyState: any;
   let dummyDispatch: any;
 
@@ -23,6 +24,7 @@ describe(`Alert`, (): void => {
     dummyTopic = { ...dummyTopicData.topic };
     dummyUser = { ...dummyUserData.user };
     dummyUpdateAlert = { ...dummyAlertData.updateAlert1, topicId: dummyTopic.id, userId: dummyUser.id };
+    dummyPullRequestAlert = { ...dummyAlertData.PRSubmittedAlert, topicId: dummyTopic.id, userId: dummyUser.id };
     dummyState = {
       ...dummyInitialState,
       modules: {
@@ -92,6 +94,52 @@ describe(`Alert`, (): void => {
 
   });
 
-  describe(`pull request alert`, (): void => { /* TODO */ });
+  describe(`pull request alerts`, (): void => {
+
+    it(`renders without errors`, (): void => {
+      const enzymeWrapper = shallow(
+        <Alert
+          {...dummyProviderProps.translatorProps}
+          alert={dummyPullRequestAlert}
+          user={dummyUser}
+          topic={dummyTopic}
+          fetchTopic={jest.fn()}
+          fetchUser={jest.fn()}
+        />,
+      );
+      expect(enzymeWrapper.isEmptyRender()).toBe(false);
+    });
+
+    it(`renders a PullRequestAlert when the passed alert prop is an alert of type PR_SUBMITTED`, (): void => {
+      const enzymeWrapper = mount(
+        <DummyProviders dummyState={dummyState} dummyDispatch={dummyDispatch}>
+          <Alert alert={{ ...dummyPullRequestAlert, type: m.alertTypes.PR_SUBMITTED }} />
+        </DummyProviders>,
+      );
+
+      expect(enzymeWrapper.find('PurePullRequestAlert')).toHaveLength(1);
+    });
+
+    it(`renders a PullRequestAlert when the passed alert prop is an alert of type PR_ACCEPTED`, (): void => {
+      const enzymeWrapper = mount(
+        <DummyProviders dummyState={dummyState} dummyDispatch={dummyDispatch}>
+          <Alert alert={{ ...dummyPullRequestAlert, type: m.alertTypes.PR_ACCEPTED }} />
+        </DummyProviders>,
+      );
+
+      expect(enzymeWrapper.find('PurePullRequestAlert')).toHaveLength(1);
+    });
+
+    it(`renders a PullRequestAlert when the passed alert prop is an alert of type PR_REJECTED`, (): void => {
+      const enzymeWrapper = mount(
+        <DummyProviders dummyState={dummyState} dummyDispatch={dummyDispatch}>
+          <Alert alert={{ ...dummyPullRequestAlert, type: m.alertTypes.PR_REJECTED }} />
+        </DummyProviders>,
+      );
+
+      expect(enzymeWrapper.find('PurePullRequestAlert')).toHaveLength(1);
+    });
+
+  });
 
 });
