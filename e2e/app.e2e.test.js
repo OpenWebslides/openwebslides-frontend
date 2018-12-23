62af7275ce7be4fde343282e552b210d53127da8
Dromin <contact@dromin.be>
2018-12-23 10:28:11 +0100

    Added very basic e2e test that checks if e2e testing is set up correctly / the app loads without errors.

---
+++ e2e/app.e2e.test.js
@@ -0,0 +1,24 @@
+// @flow
+
+/**
+ * Simple smoke test.
+ */
+
+import Nightmare from 'nightmare';
+
+import config from './config';
+
+let nightmare: typeof Nightmare;
+
+beforeEach(async (): Promise<mixed> => {
+  nightmare = new Nightmare(config.nightmareOptions);
+  await nightmare.goto(config.baseUrl);
+});
+
+afterEach(async (): Promise<mixed> => {
+  await nightmare.end();
+});
+
+test(`The app loads without error`, async (): Promise<mixed> => {
+  await nightmare.wait('[data-test-id="page"]');
+});
---
+++ e2e/config/index.js
@@ -0,0 +1,14 @@
+// @flow
+
+const config = {
+  nightmareOptions: {
+    waitTimeout: 5000,
+  },
+  baseUrl: 'http://localhost:8080',
+  signin: {
+    email: 'openwebslides.test@gmail.com',
+    password: 'owstest',
+  },
+};
+
+export default config;
