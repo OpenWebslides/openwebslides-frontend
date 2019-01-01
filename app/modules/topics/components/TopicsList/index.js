70b7326f8f18cfa67ca40189f55c7d5663583607
Dromin <contact@dromin.be>
2019-01-02 00:12:34 +0100

    Fixed TopicsList cards sizing bug.

--- app/modules/topics/components/TopicsList/index.js
+++ app/modules/topics/components/TopicsList/index.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { withNamespaces, type TranslatorProps } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import { Header, Grid, Button, Card, Icon } from 'semantic-ui-react';
+import { Header, Divider, Button, Card, Icon } from 'semantic-ui-react';
 
 import { TOPIC_NEW_ROUTE } from 'config/routes';
 
@@ -20,41 +20,38 @@ type Props = {| ...TranslatorProps, ...PassedProps |};
 const PureTopicsList = (props: Props): React.Node => {
   const { t, topicIds, isCurrentUser, onRemoveTopic } = props;
   return (
-    <Grid padded={true}>
-      <Grid.Row>
-        <div style={{ width: '100%' }}>
-          {(isCurrentUser ? (
-            <Button
-              as={Link}
-              to={TOPIC_NEW_ROUTE}
-              primary={true}
-              floated="right"
-              className="topics-list__add-button"
-              data-test-id="topics-list-add-button"
-            >
-              <Icon name="plus" />
-              {t('library:buttons.create')}
-            </Button>
-          ) : null)}
-          <Header as="h3" floated="left">{t('global:title.library')}</Header>
-        </div>
-      </Grid.Row>
-      <Grid.Row>
-        {(topicIds.length === 0 ? (
-          <em data-test-id="topics-list-empty">{t('library:noTopics')}</em>
+    <>
+      <div style={{ width: '100%', overflow: 'hidden', padding: '1rem 0 0' }}>
+        {(isCurrentUser ? (
+          <Button
+            as={Link}
+            to={TOPIC_NEW_ROUTE}
+            primary={true}
+            floated="right"
+            className="topics-list__add-button"
+            data-test-id="topics-list-add-button"
+          >
+            <Icon name="plus" />
+            {t('library:buttons.create')}
+          </Button>
         ) : null)}
-        <Card.Group itemsPerRow={3} doubling={true} stackable={true}>
-          {[...topicIds].reverse().map((topicId) => (
-            <TopicCard
-              key={topicId}
-              topicId={topicId}
-              isCurrentUser={isCurrentUser}
-              onRemoveTopic={onRemoveTopic}
-            />
-          ))}
-        </Card.Group>
-      </Grid.Row>
-    </Grid>
+        <Header as="h3" floated="left">{t('global:title.library')}</Header>
+      </div>
+      <Divider />
+      {(topicIds.length === 0 ? (
+        <em data-test-id="topics-list-empty">{t('library:noTopics')}</em>
+      ) : null)}
+      <Card.Group itemsPerRow={3} doubling={true} stackable={true}>
+        {[...topicIds].reverse().map((topicId) => (
+          <TopicCard
+            key={topicId}
+            topicId={topicId}
+            isCurrentUser={isCurrentUser}
+            onRemoveTopic={onRemoveTopic}
+          />
+        ))}
+      </Card.Group>
+    </>
   );
 };
 
