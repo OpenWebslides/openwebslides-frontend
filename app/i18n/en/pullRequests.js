0eac2692baa1db9a9cb03aa13eb200ee2a2e75dd
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-01-17 11:37:46 +0000

    Add OutgoingPullRequest component

--- app/i18n/en/pullRequests.js
+++ app/i18n/en/pullRequests.js
@@ -10,6 +10,14 @@ const pullRequests = {
   button: {
     submit: 'Send updates',
   },
+  titleForState: {
+    'pullRequestStates/PENDING': 'Pending review',
+    'pullRequestStates/READY': 'Pending review',
+    'pullRequestStates/WORKING': 'Pending review',
+    'pullRequestStates/INCOMPATIBLE': 'Incompatible',
+    'pullRequestStates/ACCEPTED': 'Accepted',
+    'pullRequestStates/REJECTED': 'Rejected',
+  },
 };
 
 export default pullRequests;
