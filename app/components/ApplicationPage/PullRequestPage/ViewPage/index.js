8c60153cd0fb0f5b163f38953ed05f78c2e2b34b
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-01-19 17:01:58 +0000

    Don't fetch current user in ViewPage

--- app/components/ApplicationPage/PullRequestPage/ViewPage/index.js
+++ app/components/ApplicationPage/PullRequestPage/ViewPage/index.js
@@ -1,84 +1,30 @@
 // @flow
 
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { type Dispatch } from 'redux';
 import { type ContextRouter as RouterProps } from 'react-router-dom';
 
-import FetchWrapper from 'components/FetchWrapper';
-import { type ModulesAction, type AppState } from 'types/redux';
 import ContainerPageWrapper from 'components/ContainerPageWrapper';
 import platform from 'modules/platform';
 import pullRequests from 'modules/pullRequests';
-import users from 'modules/users';
 
-type PassedProps = {|
-  pullRequestId: string,
-|};
-
-type StateProps = {|
-  currentUserId: ?string,
-|};
-
-type DispatchProps = {|
-  fetchUser: (id: string) => void,
-|};
-
-type Props = {| ...RouterProps, ...PassedProps, ...StateProps, ...DispatchProps |};
+type Props = {| ...RouterProps |};
 
 const { AuthWrapper } = platform.components;
 const { View } = pullRequests.components;
 
-const mapStateToProps = (state: AppState): StateProps => {
-  const userAuth = platform.selectors.getUserAuth(state);
+const PureViewPage = (props: Props): React.Node => {
+  const { match: { params: { pullRequestId } } } = props;
 
-  return {
-    currentUserId: (userAuth != null) ? userAuth.userId : null,
-  };
+  return (pullRequestId == null) ? null : (
+    <AuthWrapper>
+      <ContainerPageWrapper>
+        <View pullRequestId={pullRequestId} />
+      </ContainerPageWrapper>
+    </AuthWrapper>
+  );
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<ModulesAction>): DispatchProps => {
-  return {
-    fetchUser: (id: string): void => {
-      dispatch(users.actions.fetch(id));
-    },
-  };
-};
-
-class PureViewPage extends React.Component<Props> {
-  renderViewPage = (currentUser: users.model.User): React.Node => {
-    const { match: { params: { pullRequestId } } } = this.props;
-
-    return (pullRequestId == null) ? null : (
-      <div data-test-id="view-page">
-        <AuthWrapper>
-          <ContainerPageWrapper>
-            <View
-              pullRequestId={pullRequestId}
-              currentUser={currentUser}
-            />
-          </ContainerPageWrapper>
-        </AuthWrapper>
-      </div>
-    );
-  };
-
-  render(): React.Node {
-    const { currentUserId } = this.props;
-
-    return (
-      <FetchWrapper
-        render={this.renderViewPage}
-        renderPropsAndState={this.props}
-        fetchId={currentUserId}
-        fetchAction={users.actions.fetch}
-        fetchedPropSelector={users.selectors.getById}
-      />
-    );
-  }
-}
-
-const ViewPage = connect(mapStateToProps, mapDispatchToProps)(PureViewPage);
+const ViewPage = PureViewPage;
 
 export { PureViewPage };
 export default ViewPage;
--- app/components/ApplicationPage/PullRequestPage/ViewPage/index.test.js
+++ app/components/ApplicationPage/PullRequestPage/ViewPage/index.test.js
@@ -2,57 +2,19 @@
 
 import _ from 'lodash';
 import * as React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
-import { DummyProviders, dummyInitialState, dummyUserData, dummyPullRequestData, dummyTopicData, dummyProviderProps } from 'lib/testResources';
-import users from 'modules/users';
+import { dummyPullRequestData, dummyProviderProps } from 'lib/testResources';
 import pullRequests from 'modules/pullRequests';
 
-import ViewPage, { PureViewPage } from '.';
+import { PureViewPage } from '.';
 
 describe(`ViewPage`, (): void => {
 
   let dummyPullRequest: pullRequests.model.PullRequest;
-  let dummyCurrentUser: users.model.User;
-  let dummyState: any;
-  let dummyDispatch: any;
 
   beforeEach((): void => {
     dummyPullRequest = { ...dummyPullRequestData.pullRequest };
-    dummyCurrentUser = { ...dummyUserData.user };
-    dummyState = {
-      ...dummyInitialState,
-      modules: {
-        ...dummyInitialState.modules,
-        platform: {
-          ...dummyInitialState.modules.platform,
-          userAuth: {
-            userId: dummyCurrentUser.id,
-            apiToken: 'foobarToken',
-          },
-        },
-        users: {
-          ...dummyInitialState.modules.users,
-          byId: {
-            [dummyCurrentUser.id]: dummyCurrentUser,
-          },
-        },
-        pullRequests: {
-          ...dummyInitialState.modules.pullRequests,
-          byId: {
-            [dummyPullRequest.id]: dummyPullRequest,
-          },
-        },
-        topics: {
-          ...dummyInitialState.modules.topics,
-          byId: {
-            [dummyPullRequest.sourceTopicId]: dummyTopicData.topic,
-            [dummyPullRequest.targetTopicId]: dummyTopicData.topic,
-          },
-        },
-      },
-    };
-    dummyDispatch = jest.fn();
   });
 
   it(`renders without errors`, (): void => {
@@ -65,48 +27,12 @@ describe(`ViewPage`, (): void => {
     expect(enzymeWrapper.isEmptyRender()).toBe(false);
   });
 
-  it(`renders no components when match.params.pullRequestId is NULL`, (): void => {
+  it(`renders NULL when match.params.pullRequestId is NULL`, (): void => {
     const enzymeWrapper = shallow(
       <PureViewPage {...dummyProviderProps.routerProps} />,
     );
 
-    // Cannot asset to be empty render, due to FetchWrapper
-    expect(enzymeWrapper.find('[data-test-id="view-page"]').hostNodes()).toHaveLength(0);
-  });
-
-  it(`loads the current user, when the current user was not previously present in the state`, (): void => {
-    const fixedRouterProps = _.set(_.cloneDeep(dummyProviderProps.routerProps), 'match.params.pullRequestId', dummyPullRequest.id);
-
-    dummyState.modules.users.byId = {};
-
-    const enzymeWrapper = mount(
-      <DummyProviders dummyState={dummyState} dummyDispatch={dummyDispatch}>
-        <ViewPage
-          {...fixedRouterProps}
-          pullRequestId={dummyPullRequest.id}
-        />
-      </DummyProviders>,
-    );
-
-    expect(dummyDispatch).toHaveBeenCalledWith(users.actions.fetch(dummyCurrentUser.id));
-    expect(enzymeWrapper.find('[data-test-id="view-page"]').hostNodes()).toHaveLength(0);
-  });
-
-  it(`renders the pull request view, when the pull request was previously present in the state`, (): void => {
-    const fixedRouterProps = _.set(_.cloneDeep(dummyProviderProps.routerProps), 'match.params.pullRequestId', dummyPullRequest.id);
-
-    const enzymeWrapper = mount(
-      <DummyProviders dummyState={dummyState} dummyDispatch={dummyDispatch}>
-        <ViewPage
-          {...fixedRouterProps}
-          pullRequestId={dummyPullRequest.id}
-        />
-      </DummyProviders>,
-    );
-
-    // FIXME: Called two times by external components
-    expect(dummyDispatch).toHaveBeenCalledTimes(2);
-    expect(enzymeWrapper.find('[data-test-id="view-page"]').hostNodes()).toHaveLength(1);
+    expect(enzymeWrapper.isEmptyRender()).toBe(true);
   });
 
 });
