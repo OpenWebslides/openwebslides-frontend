930f38fc603c11b0f4a0ba13b5e2be87a6aee6da
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-01-24 15:55:41 +0100

    Fix enabled -> supported typo

--- app/i18n/en/settings.js
+++ app/i18n/en/settings.js
@@ -17,7 +17,7 @@ const settings = {
     close: {
       title: 'Close account',
       info: 'Closing your account will disable your access to the platform, in addition to removing all your content (topics, annotations and comments). Please make sure this is what you really want!',
-      disabled: 'Closing your account is currently not enabled. Please contact your system administrator.',
+      disabled: 'Closing your account is currently not supported. Please contact your system administrator.',
       button: 'Close account',
     },
   },
