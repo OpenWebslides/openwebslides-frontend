8ed2566ae282f2b4628b175091c965b709c477ba
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-01-30 11:11:52 +0100

    Render ConditionalWrapper in PolicyWrapper

--- app/modules/policies/components/PolicyWrapper/index.js
+++ app/modules/policies/components/PolicyWrapper/index.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import FetchWrapper from 'components/FetchWrapper';
+import ConditionalWrapper from 'components/ConditionalWrapper';
 import { type AppState } from 'types/redux';
 import platform from 'modules/platform';
 import users from 'modules/users';
@@ -15,6 +16,12 @@ type PassedProps<T> = {|
   policy: policies.Policy<T>,
   action: string,
   children: React.Node,
+  // Optional route to which the user will be redirected, if the user is not authenticated.
+  // Defaults to SIGNIN_ROUTE. Redirecting can be disabled by setting this to NULL.
+  redirectIfNotAuthenticated: ?string,
+  // Optional component that will be rendered instead of the children,
+  // if the user is not authenticated.
+  componentIfNotAuthenticated: ?React.ComponentType<{}>,
 |};
 
 type StateProps = {|
@@ -33,12 +40,27 @@ const mapStateToProps = (state: AppState): StateProps => {
 
 class PurePolicyWrapper<T> extends React.Component<Props<T>> {
   renderPolicyWrapper = (currentUser: users.model.User): React.Node => {
-    const { record, policy: PolicyClass, action, children } = this.props;
+    const {
+      record,
+      policy: PolicyClass,
+      action,
+      children,
+      redirectIfNotAuthenticated,
+      componentIfNotAuthenticated,
+    } = this.props;
 
     // $FlowFixMe I have no idea how to FlowFix this
     const policy = new PolicyClass(currentUser, record);
 
-    return policy[action]() ? children : null;
+    return (
+      <ConditionalWrapper
+        renderChildren={policy[action]()}
+        componentIfNotChildren={componentIfNotAuthenticated}
+        redirectIfNotChildren={redirectIfNotAuthenticated}
+      >
+        {children}
+      </ConditionalWrapper>
+    );
   };
 
   render(): React.Node {
