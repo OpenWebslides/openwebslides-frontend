ab6831d8a3254c40be5fe13573f55c9dc96a1e62
Dromin <contact@dromin.be>
2019-02-03 12:46:30 +0100

    Updated contentItems remove saga to automatically generate a new placeholder contentItem if the last contentItem in the topic was removed.

--- app/modules/contentItems/saga/task/remove.js
+++ app/modules/contentItems/saga/task/remove.js
@@ -5,7 +5,7 @@
 import { type Saga } from 'redux-saga';
 import { call, put, select } from 'redux-saga/effects';
 
-import { CorruptedInternalStateError, ObjectNotFoundError } from 'errors';
+import { CorruptedInternalStateError, ObjectNotFoundError, UnsupportedOperationError } from 'errors';
 import asyncRequests from 'modules/asyncRequests';
 
 import actions from '../../actions';
@@ -16,8 +16,10 @@ import selectors from '../../selectors';
 
 const { putAndReturn } = asyncRequests.lib;
 
-const removeHeading = function* (headingToRemove: m.HeadingContentItem): Saga<void> {
-  const contentItemsById = yield select(selectors.getAllById);
+const removeHeading = function* (
+  headingToRemove: m.HeadingContentItem,
+  contentItemsById: m.ContentItemsById,
+): Saga<void> {
   const previousSiblingItem = lib.find.previousSiblingItem(headingToRemove, contentItemsById);
   let moveContext: m.VerticalContext;
 
@@ -52,18 +54,46 @@ const removeHeading = function* (headingToRemove: m.HeadingContentItem): Saga<vo
   }
 };
 
+const generatePlaceholderIfRootEmpty = function* (rootContentItemId: string): Saga<void> {
+  // Get the updated ROOT, after the remove has been completed.
+  const rootContentItem = yield select(selectors.getById, { id: rootContentItemId });
+
+  // If the ROOT is empty, generate a placeholder item.
+  if (rootContentItem.childItemIds.length === 0) {
+    yield call(putAndReturn, actions.generatePlaceholder(rootContentItemId));
+  }
+};
+
 const remove = function* (action: a.RemoveAction): Saga<void> {
   const { id } = action.payload;
   const contentItemToRemove = yield select(selectors.getById, { id });
   if (contentItemToRemove == null) throw new ObjectNotFoundError('contentItems:contentItem', id);
 
+  // Verify that the contentItemToRemove isn't the ROOT, since the ROOT should not be removable.
+  if (contentItemToRemove.type === m.contentItemTypes.ROOT) throw new UnsupportedOperationError('Cannot remove a ROOT contentItem.');
+
+  // Find the ROOT before removing the contentItem, since this won't be possible afterwards.
+  const contentItemsById = yield select(selectors.getAllById);
+  const rootContentItem = lib.find.closest(
+    contentItemToRemove,
+    contentItemsById,
+    lib.find.parentOrSuperItem,
+    (contentItem: m.ContentItem) => (contentItem.type === m.contentItemTypes.ROOT),
+  );
+  if (rootContentItem == null) throw new CorruptedInternalStateError('Invalid contentItemsById: could not find ROOT.');
+
   // If the contentItem is a HEADING, take care of its subItems first.
   if (contentItemToRemove.type === m.contentItemTypes.HEADING) {
-    yield call(removeHeading, contentItemToRemove);
+    yield call(removeHeading, contentItemToRemove, contentItemsById);
   }
 
   // Then, remove the contentItem.
   yield put(actions.removeFromState(id));
+
+  // If the removed contentItem was the last child of the ROOT,
+  // generate a new placeholder so that the user still has a place to continue editing.
+  yield call(generatePlaceholderIfRootEmpty, rootContentItem.id);
 };
 
+export { removeHeading };
 export default remove;
