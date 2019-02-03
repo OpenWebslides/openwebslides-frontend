d5cf0f40f6dbeee1a279e866d45e8f5a204e5691
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-02-03 17:43:45 +0100

    Add missing flash messages for platform actions

--- app/i18n/en/flash.js
+++ app/i18n/en/flash.js
@@ -14,6 +14,16 @@ const flash = {
     success: 'Welcome aboard! You can now sign in using the email address and password you provided',
     error: 'An error occurred while confirming your account. Have you already confirmed your account?',
   },
+  'platform/RESEND_CONFIRMATION_EMAIL': {
+    success: 'An email with a link to confirm your account was sent to your email address',
+  },
+  'platform/RESET_PASSWORD': {
+    success: 'Your password was reset! You can now sign in again',
+    error: 'An error occurred while resetting your password',
+  },
+  'platform/SEND_RESET_PASSWORD_EMAIL': {
+    success: 'An email with a link to reset your password was sent to your email address',
+  },
   'pullRequests/CREATE': {
     success: 'Successfully shared your contribution',
     error: 'An error occurred while sharing your contribution',
