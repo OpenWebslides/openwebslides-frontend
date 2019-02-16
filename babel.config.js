640a618391421fcdf27e63df42db007a046a5b46
Dromin <contact@dromin.be>
2019-02-16 19:18:25 +0100

    Fixed error caused by missing `regenerator-runtime`.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 // @flow
 
 const presets = [
-  '@babel/preset-env',
+  [
+    '@babel/preset-env',
+    {
+      targets: {
+        node: 'current',
+      },
+    },
+  ],
   '@babel/preset-react',
   '@babel/preset-flow',
 ];
