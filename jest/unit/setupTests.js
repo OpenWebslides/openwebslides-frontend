1368b61d4619153e5034b24d49e092260f44c68d
Dromin <contact@dromin.be>
2019-02-18 12:25:56 +0100

    Mock react-i18next useTranslation() hook in tests to prevent warnings when shallow rendering components using this hook.

--- jest/unit/setupTests.js
+++ jest/unit/setupTests.js
@@ -4,9 +4,19 @@
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as reactI18next from 'react-i18next';
 
-// Create Enzyme adapter.
+// Create Enzyme adapter
 configure({ adapter: new Adapter() });
 
 // Mock fetch API
 global.fetch = require('jest-fetch-mock');
+
+// Mock react-i18next useTranslation() hook so that shallow rendering doesn't throw a warning
+jest.doMock('react-i18next', (): mixed => ({
+  ...reactI18next,
+  useTranslation: (): $ReadOnlyArray<mixed> => ([
+    (key: string): string => key,
+    { languages: [] },
+  ]),
+}));
