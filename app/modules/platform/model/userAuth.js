afdd3c25462c86778a44e6f407e1070816701f99
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-11 10:09:51 -0300

    Replace userAuth.token by .accessToken and .refreshToken

--- app/modules/platform/model/userAuth.js
+++ app/modules/platform/model/userAuth.js
@@ -2,5 +2,10 @@
 
 export type UserAuth = {|
   +userId: string,
-  +apiToken: string,
+  // Long-lived refresh token, used only for obtaining a short-lived access token
+  // See https://github.com/floriandejonckheere/jwt-auth for a summary of the refresh token mechanism
+  +refreshToken: string,
+  // Short-lived refresh token, used for API requests
+  // See https://github.com/floriandejonckheere/jwt-auth for a summary of the refresh token mechanism
+  +accessToken: string,
 |};
