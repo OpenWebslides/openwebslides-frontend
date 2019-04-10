1b20e934c76c73a0a267628b14aa41278fa0fe5e
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-04-10 10:43:04 +0200

    Replace switch with if to get rid of redundant default: statement

--- app/modules/contentItems/reducer/selectInState.js
+++ app/modules/contentItems/reducer/selectInState.js
@@ -15,36 +15,31 @@ const selectInState = (
 
   let newContentItem: ?m.ContentItem;
 
-  switch (action.payload.selection) {
-    case m.selectionTypes.PARENT:
-      // Select parent of current contentItem
-      newContentItem = lib.find.superItem(currentContentItem, state.byId);
-      break;
-    case m.selectionTypes.CHILD:
-      // Select first child of current contentItem
-      newContentItem = lib.find.allSubItems(currentContentItem, state.byId)[0];
-      break;
-    case m.selectionTypes.NEXT:
-      // Select next sibling of current contentItem
-      newContentItem = lib.find.nextSiblingItem(currentContentItem, state.byId);
+  if (action.payload.selection === m.selectionTypes.PARENT) {
+    // Select parent of current contentItem
+    newContentItem = lib.find.superItem(currentContentItem, state.byId);
+  }
+  else if (action.payload.selection === m.selectionTypes.CHILD) {
+    // Select first child of current contentItem
+    newContentItem = lib.find.allSubItems(currentContentItem, state.byId)[0];
+  }
+  else if (action.payload.selection === m.selectionTypes.NEXT) {
+    // Select next sibling of current contentItem
+    newContentItem = lib.find.nextSiblingItem(currentContentItem, state.byId);
 
-      // Or next item in editor order if there are no more siblings
-      if (newContentItem == null) {
-        newContentItem = lib.find.nextEditorItem(currentContentItem, state.byId);
-      }
+    // Or next item in editor order if there are no more siblings
+    if (newContentItem == null) {
+      newContentItem = lib.find.nextEditorItem(currentContentItem, state.byId);
+    }
+  }
+  else if (action.payload.selection === m.selectionTypes.PREVIOUS) {
+    // Select previous sibling of current contentItem
+    newContentItem = lib.find.previousSiblingItem(currentContentItem, state.byId);
 
-      break;
-    case m.selectionTypes.PREVIOUS:
-      // Select previous sibling of current contentItem
-      newContentItem = lib.find.previousSiblingItem(currentContentItem, state.byId);
-
-      // Or previous item in editor order if there are no more siblings
-      if (newContentItem == null) {
-        newContentItem = lib.find.previousEditorItem(currentContentItem, state.byId);
-      }
-      break;
-    default:
-      return state;
+    // Or previous item in editor order if there are no more siblings
+    if (newContentItem == null) {
+      newContentItem = lib.find.previousEditorItem(currentContentItem, state.byId);
+    }
   }
 
   return (newContentItem == null ? state : { ...state, currentlySelectedId: newContentItem.id });
