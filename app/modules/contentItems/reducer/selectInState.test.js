6adf4f2d13ea6c0f2f729abdb03bd105627c5792
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-04-10 09:22:13 +0200

    Group tests by passed argument description

--- app/modules/contentItems/reducer/selectInState.test.js
+++ app/modules/contentItems/reducer/selectInState.test.js
@@ -50,160 +50,176 @@ describe(`selectInState`, (): void => {
     expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
   });
 
-  it(`sets the parent or super contentItem as currently selected id in the state when PARENT is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem1.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.PARENT,
-      },
-    };
-    const nextState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItemParent.id,
-    };
+  describe(`when PARENT is passed as selectionType`, (): void => {
 
-    const resultState = reducer(prevState, selectInStateAction);
-    expect(resultState).toStrictEqual(nextState);
-    expect(resultState).not.toBe(prevState);
-    expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    it(`sets the parent or super contentItem as currently selected contentItem in the state`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem1.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.PARENT,
+        },
+      };
+      const nextState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItemParent.id,
+      };
+
+      const resultState = reducer(prevState, selectInStateAction);
+      expect(resultState).toStrictEqual(nextState);
+      expect(resultState).not.toBe(prevState);
+      expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    });
+
+    it(`leaves the state unchanged, when the currently selected contentItem has no parent`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItemParent.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.PARENT,
+        },
+      };
+      const resultState = reducer(prevState, selectInStateAction);
+
+      expect(resultState).toBe(prevState);
+      expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
+    });
+
   });
 
-  it(`leaves the state unchanged, when the currently selected contentItem has no parent when PARENT is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItemParent.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.PARENT,
-      },
-    };
-    const resultState = reducer(prevState, selectInStateAction);
+  describe(`when CHILD is passed as selectionType`, (): void => {
 
-    expect(resultState).toBe(prevState);
-    expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
+    it(`sets the first child or sub contentItem as currently selected contentItem in the state`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItemParent.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.CHILD,
+        },
+      };
+      const nextState: m.ContentItemsState = {
+        ...prevState,
+        currentlySelectedId: dummyContentItem1.id,
+      };
+
+      const resultState = reducer(prevState, selectInStateAction);
+      expect(resultState).toStrictEqual(nextState);
+      expect(resultState).not.toBe(prevState);
+      expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    });
+
+    it(`leaves the state unchanged, when the currently selected contentItem has no children or sub contentItems`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem1.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.CHILD,
+        },
+      };
+      const resultState = reducer(prevState, selectInStateAction);
+
+      expect(resultState).toBe(prevState);
+      expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
+    });
+
   });
 
-  it(`sets the first child or sub contentItem as currently selected id in the state when CHILD is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItemParent.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.CHILD,
-      },
-    };
-    const nextState: m.ContentItemsState = {
-      ...prevState,
-      currentlySelectedId: dummyContentItem1.id,
-    };
+  describe(`when PREVIOUS is passed as selectionType`, (): void => {
 
-    const resultState = reducer(prevState, selectInStateAction);
-    expect(resultState).toStrictEqual(nextState);
-    expect(resultState).not.toBe(prevState);
-    expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    it(`sets the previous sibling as currently selected contentItem in the state`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem2.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.PREVIOUS,
+        },
+      };
+      const nextState: m.ContentItemsState = {
+        ...prevState,
+        currentlySelectedId: dummyContentItem1.id,
+      };
+
+      const resultState = reducer(prevState, selectInStateAction);
+      expect(resultState).toStrictEqual(nextState);
+      expect(resultState).not.toBe(prevState);
+      expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    });
+
+    it(`leaves the state unchanged, when the currently selected contentItem has no previous sibling`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem1.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.PREVIOUS,
+        },
+      };
+      const resultState = reducer(prevState, selectInStateAction);
+
+      expect(resultState).toBe(prevState);
+      expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
+    });
+
   });
 
-  it(`leaves the state unchanged, when the currently selected contentItem has no children or sub contentItems when CHILD is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem1.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.CHILD,
-      },
-    };
-    const resultState = reducer(prevState, selectInStateAction);
+  describe(`when NEXT is passed as selectionType`, (): void => {
 
-    expect(resultState).toBe(prevState);
-    expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
-  });
+    it(`sets the next sibling as currently selected contentItem in the state`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem2.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.NEXT,
+        },
+      };
+      const nextState: m.ContentItemsState = {
+        ...prevState,
+        currentlySelectedId: dummyContentItem3.id,
+      };
 
-  it(`sets the previous sibling as currently selected id in the state when PREVIOUS is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem2.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.PREVIOUS,
-      },
-    };
-    const nextState: m.ContentItemsState = {
-      ...prevState,
-      currentlySelectedId: dummyContentItem1.id,
-    };
+      const resultState = reducer(prevState, selectInStateAction);
+      expect(resultState).toStrictEqual(nextState);
+      expect(resultState).not.toBe(prevState);
+      expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
+    });
 
-    const resultState = reducer(prevState, selectInStateAction);
-    expect(resultState).toStrictEqual(nextState);
-    expect(resultState).not.toBe(prevState);
-    expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
-  });
+    it(`leaves the state unchanged, when the currently selected contentItem has no next sibling`, (): void => {
+      const prevState: m.ContentItemsState = {
+        byId: dummyContentItemsById,
+        currentlySelectedId: dummyContentItem3.id,
+      };
+      const selectInStateAction: a.SelectInStateAction = {
+        type: a.SELECT_IN_STATE,
+        payload: {
+          selection: m.selectionTypes.NEXT,
+        },
+      };
+      const resultState = reducer(prevState, selectInStateAction);
 
-  it(`leaves the state unchanged, when the currently selected contentItem has no previous sibling when PREVIOUS is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem1.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.PREVIOUS,
-      },
-    };
-    const resultState = reducer(prevState, selectInStateAction);
+      expect(resultState).toBe(prevState);
+      expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
+    });
 
-    expect(resultState).toBe(prevState);
-    expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
-  });
-
-  it(`sets the previous sibling as currently selected id in the state when NEXT is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem2.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.NEXT,
-      },
-    };
-    const nextState: m.ContentItemsState = {
-      ...prevState,
-      currentlySelectedId: dummyContentItem3.id,
-    };
-
-    const resultState = reducer(prevState, selectInStateAction);
-    expect(resultState).toStrictEqual(nextState);
-    expect(resultState).not.toBe(prevState);
-    expect(resultState.currentlySelectedId).not.toBe(prevState.currentlySelectedId);
-  });
-
-  it(`leaves the state unchanged, when the currently selected contentItem has no previous sibling when NEXT is passed`, (): void => {
-    const prevState: m.ContentItemsState = {
-      byId: dummyContentItemsById,
-      currentlySelectedId: dummyContentItem3.id,
-    };
-    const selectInStateAction: a.SelectInStateAction = {
-      type: a.SELECT_IN_STATE,
-      payload: {
-        selection: m.selectionTypes.NEXT,
-      },
-    };
-    const resultState = reducer(prevState, selectInStateAction);
-
-    expect(resultState).toBe(prevState);
-    expect(resultState.currentlySelectedId).toBe(prevState.currentlySelectedId);
   });
 
 });
