8746ba681054e630d0c8dadf228c3fc7ac9c6d65
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-04-19 11:51:53 +0200

    Synchronize selection in state with contentItem browser focus on TypeBlockWrapper

--- app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.js
+++ app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.js
@@ -4,6 +4,8 @@ import * as React from 'react';
 import { Icon } from 'semantic-ui-react';
 
 type PassedProps = {|
+  onFocus: (event: SyntheticFocusEvent<HTMLElement>) => void,
+  onBlur: (event: SyntheticFocusEvent<HTMLElement>) => void,
   iconName: string,
   isSelected: boolean,
   children?: React.Node,
@@ -11,26 +13,52 @@ type PassedProps = {|
 
 type Props = {| ...PassedProps |};
 
-const PureTypeBlockWrapper = (props: Props): React.Node => {
-  const { iconName, isSelected, children } = props;
+class PureTypeBlockWrapper extends React.Component<Props> {
+  static defaultProps = {
+    children: null,
+  };
 
-  return (
-    <div className={`content-item-editable-display-block ${isSelected ? 'content-item-editable-display-block--selected' : ''}`}>
-      <div className="content-item-editable-display-block__wrapper">
-        <div className="content-item-editable-display-block__icon">
-          <Icon name={iconName} color="grey" />
-        </div>
-        <div className="content-item-editable-display-block__content">
-          { children }
+  componentDidUpdate(): void {
+    const { isSelected } = this.props;
+
+    if (this.blockRef == null) return;
+
+    if (isSelected) this.blockRef.focus();
+    else this.blockRef.blur();
+  }
+
+  handleRef = (c: ?HTMLDivElement): void => {
+    this.blockRef = c;
+  };
+
+  blockRef: ?HTMLDivElement;
+
+  render(): React.Node {
+    const { isSelected, onFocus, onBlur, iconName, children } = this.props;
+
+    return (
+      /* eslint-disable jsx-a11y/click-events-have-key-events */
+      <div
+        className={`content-item-editable-display-block ${isSelected ? 'content-item-editable-display-block--selected' : ''}`}
+        role="link"
+        tabIndex={0}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        ref={this.handleRef}
+        data-test-id="type-block-wrapper"
+      >
+        <div className="content-item-editable-display-block__wrapper">
+          <div className="content-item-editable-display-block__icon">
+            <Icon name={iconName} color="grey" />
+          </div>
+          <div className="content-item-editable-display-block__content">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-PureTypeBlockWrapper.defaultProps = {
-  children: null,
-};
+    );
+  }
+}
 
 const TypeBlockWrapper = PureTypeBlockWrapper;
 
--- app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.test.js
+++ app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.test.js
@@ -11,17 +11,25 @@ describe(`TypeBlockWrapper`, (): void => {
   let dummyText: string;
   let DummyChildComponent: () => React.Node;
 
+  let dummyOnFocus: any;
+  let dummyOnBlur: any;
+  let dummyBlockRef: any;
+
   beforeEach((): void => {
     dummyIconName = 'paragraph';
     dummyText = 'Lorem ipsum dolor sit amet.';
     DummyChildComponent = (): React.Node => (
       <p>{dummyText}</p>
     );
+
+    dummyOnFocus = jest.fn();
+    dummyOnBlur = jest.fn();
+    dummyBlockRef = { focus: jest.fn(), blur: jest.fn() };
   });
 
   it(`renders without errors`, (): void => {
     const enzymeWrapper = shallow(
-      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false}>
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
         <DummyChildComponent />
       </PureTypeBlockWrapper>,
     );
@@ -30,7 +38,7 @@ describe(`TypeBlockWrapper`, (): void => {
 
   it(`renders an icon with the given iconName`, (): void => {
     const enzymeWrapper = mount(
-      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false}>
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
         <DummyChildComponent />
       </PureTypeBlockWrapper>,
     );
@@ -39,7 +47,7 @@ describe(`TypeBlockWrapper`, (): void => {
 
   it(`attaches a --selected modifier when the passed isSelected is TRUE`, (): void => {
     const enzymeWrapper = mount(
-      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={true}>
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={true} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
         <DummyChildComponent />
       </PureTypeBlockWrapper>,
     );
@@ -48,7 +56,7 @@ describe(`TypeBlockWrapper`, (): void => {
 
   it(`does not attach a --selected modifier when the passed isSelected is FALSE`, (): void => {
     const enzymeWrapper = mount(
-      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false}>
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
         <DummyChildComponent />
       </PureTypeBlockWrapper>,
     );
@@ -57,11 +65,65 @@ describe(`TypeBlockWrapper`, (): void => {
 
   it(`renders its children`, (): void => {
     const enzymeWrapper = mount(
-      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false}>
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
         <DummyChildComponent />
       </PureTypeBlockWrapper>,
     );
     expect(enzymeWrapper.find(DummyChildComponent)).toHaveLength(1);
+  });
+
+  it(`calls the passed onFocus function when the component is focused`, (): void => {
+    const enzymeWrapper = mount(
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
+        <DummyChildComponent />
+      </PureTypeBlockWrapper>,
+    );
+    enzymeWrapper.find('[data-test-id="type-block-wrapper"]').hostNodes().simulate('focus');
+    expect(dummyOnFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it(`calls the passed onBlur function when the component is blurred`, (): void => {
+    const enzymeWrapper = mount(
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
+        <DummyChildComponent />
+      </PureTypeBlockWrapper>,
+    );
+    enzymeWrapper.find('[data-test-id="type-block-wrapper"]').hostNodes().simulate('blur');
+    expect(dummyOnBlur).toHaveBeenCalledTimes(1);
+  });
+
+  it(`focuses the component when the contentItem is selected`, (): void => {
+    const enzymeWrapper = mount(
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={false} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
+        <DummyChildComponent />
+      </PureTypeBlockWrapper>,
+    );
+
+    enzymeWrapper.instance().blockRef = dummyBlockRef;
+
+    enzymeWrapper.setProps({ isSelected: true });
+
+    // Enzyme does not support event propagation yet, so we cannot test out
+    // the onFocus callback, but we can test the reference focus() call
+    expect(dummyBlockRef.focus).toHaveBeenCalledTimes(1);
+    expect(dummyBlockRef.blur).not.toHaveBeenCalled();
+  });
+
+  it(`blurs the component when the contentItem is deselected`, (): void => {
+    const enzymeWrapper = mount(
+      <PureTypeBlockWrapper iconName={dummyIconName} isSelected={true} onFocus={dummyOnFocus} onBlur={dummyOnBlur}>
+        <DummyChildComponent />
+      </PureTypeBlockWrapper>,
+    );
+
+    enzymeWrapper.instance().blockRef = dummyBlockRef;
+
+    enzymeWrapper.setProps({ isSelected: false });
+
+    // Enzyme does not support event propagation yet, so we cannot test out
+    // the onFocus callback, but we can test the reference focus() call
+    expect(dummyBlockRef.focus).not.toHaveBeenCalled();
+    expect(dummyBlockRef.blur).toHaveBeenCalledTimes(1);
   });
 
 });
