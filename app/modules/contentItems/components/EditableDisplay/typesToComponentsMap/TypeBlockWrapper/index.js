08216bc9d291b47c8fcaf70775d11623b91aab02
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-05-02 09:34:56 +0200

    Display drag handles on type block hover

--- app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.js
+++ app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/TypeBlockWrapper/index.js
@@ -12,11 +12,19 @@ type PassedProps = {|
   children?: React.Node,
 |};
 
+type ComponentState = {|
+  isHovering: boolean,
+|};
+
 type Props = {| ...PassedProps |};
 
-class PureTypeBlockWrapper extends React.Component<Props> {
+class PureTypeBlockWrapper extends React.Component<Props, ComponentState> {
   static defaultProps = {
     children: null,
+  };
+
+  state: ComponentState = {
+    isHovering: false,
   };
 
   handleFocus = (): void => {
@@ -29,8 +37,17 @@ class PureTypeBlockWrapper extends React.Component<Props> {
     onBlur();
   };
 
+  handleMouseEnter = (): void => {
+    this.setState({ isHovering: true });
+  };
+
+  handleMouseLeave = (): void => {
+    this.setState({ isHovering: false });
+  };
+
   render(): React.Node {
-    const { isSelected, iconName, children } = this.props;
+    const { contentItemId, isSelected, iconName, children } = this.props;
+    const { isHovering } = this.state;
 
     return (
       /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -40,11 +57,17 @@ class PureTypeBlockWrapper extends React.Component<Props> {
         tabIndex={0}
         onFocus={this.handleFocus}
         onBlur={this.handleBlur}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
         data-test-id="type-block-wrapper"
       >
         <div className="content-item-editable-display-block__wrapper">
           <div className="content-item-editable-display-block__icon">
-            <Icon name={iconName} color="grey" />
+            {isHovering ? (
+              <Icon name="bars" color="grey" />
+            ) : (
+              <Icon name={iconName} color="grey" />
+            )}
           </div>
           <div className="content-item-editable-display-block__content">
             {children}
