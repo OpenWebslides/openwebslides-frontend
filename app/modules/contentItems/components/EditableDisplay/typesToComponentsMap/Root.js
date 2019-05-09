4c708924c6ca907ad131a399cf0fbf469518a71c
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-05-09 14:18:13 +0200

    Revert "Root editable content item: render NULL"
    This reverts commit c03375119b0db348f23bdff347812560b089902c.

--- app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/Root.js
+++ app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/Root.js
@@ -1,8 +1,11 @@
 // @flow
 
+import _ from 'lodash';
 import * as React from 'react';
 
 import * as m from '../../../model';
+
+import ContentItemEditableDisplay, { passThroughProps } from '..';
 
 type PassedProps = {|
   contentItem: m.RootContentItem,
@@ -10,7 +13,23 @@ type PassedProps = {|
 
 type Props = {| ...PassedProps |};
 
-const PureRoot = (props: Props): React.Node => null;
+const PureRoot = (props: Props): React.Node => {
+  const { contentItem } = props;
+
+  return (
+    <div data-test-id="content-item-editable-display-root">
+      {contentItem.subItemIds.map((subItemId: string): React.Node => {
+        return (
+          <ContentItemEditableDisplay
+            {..._.pick(props, passThroughProps)}
+            key={subItemId}
+            contentItemId={subItemId}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 const Root = PureRoot;
 
--- app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/Root.test.js
+++ app/modules/contentItems/components/EditableDisplay/typesToComponentsMap/Root.test.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { mount, shallow } from 'enzyme';
 
-import { dummyContentItemData as dummyData } from 'lib/testResources';
+import { DummyProviders, dummyInitialState, dummyContentItemData as dummyData } from 'lib/testResources';
 
 import * as m from '../../../model';
 
@@ -14,25 +14,44 @@ describe(`Root`, (): void => {
   let dummyHeading2: m.HeadingContentItem;
   let dummyHeading1: m.HeadingContentItem;
   let dummyRoot: m.RootContentItem;
+  let dummyContentItemsById: m.ContentItemsById;
+  let dummyState: any;
 
   beforeEach((): void => {
     dummyHeading2 = { ...dummyData.headingContentItem2 };
     dummyHeading1 = { ...dummyData.headingContentItem };
     dummyRoot = { ...dummyData.rootContentItem, subItemIds: [dummyHeading1.id, dummyHeading2.id] };
+    dummyContentItemsById = {
+      [dummyRoot.id]: dummyRoot,
+      [dummyHeading1.id]: dummyHeading1,
+      [dummyHeading2.id]: dummyHeading2,
+    };
+    dummyState = {
+      ...dummyInitialState,
+      modules: {
+        ...dummyInitialState.modules,
+        contentItems: {
+          ...dummyInitialState.modules.contentItems,
+          byId: dummyContentItemsById,
+        },
+      },
+    };
   });
 
   it(`renders without errors`, (): void => {
     const enzymeWrapper = shallow(
       <PureRoot contentItem={dummyRoot} />,
     );
-    expect(enzymeWrapper.isEmptyRender()).toBe(true);
+    expect(enzymeWrapper.isEmptyRender()).toBe(false);
   });
 
-  it(`renders nothing`, (): void => {
+  it(`renders all of its sub items`, (): void => {
     const enzymeWrapper = mount(
-      <PureRoot contentItem={dummyRoot} />,
+      <DummyProviders dummyState={dummyState}>
+        <PureRoot contentItem={dummyRoot} />
+      </DummyProviders>,
     );
-    expect(enzymeWrapper.isEmptyRender()).toBe(true);
+    expect(enzymeWrapper.find('PureHeading')).toHaveLength(2);
   });
 
 });
